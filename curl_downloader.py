7e65eb6dd11d18248a5ffb8beef1c5b7ac2c4a46
zpx2012 <zhupengxiong@yeah.net>
2018-07-15 22:48:51 -0700

    add curl_downloader

---
+++ curl_downloader.py
@@ -0,0 +1,37 @@
+import os,sys,time,datetime,socket,shlex
+from subprocess import Popen, PIPE
+from time import sleep
+from os.path import expanduser
+
+if __name__ == '__main__':
+    if len(sys.argv) < 5:
+        print("Usage:\n\tpython curl_downloader.py [URL] [Mode] [Tool] [Mirror]\n\nOptions:\n\tMode:0:regular, 1:proxy mode\n\tTool: ss, vpn, ssh, from which server\n\tMirror: mirror source, 163 or mit or so")
+        sys.exit(-1)
+
+    print '********************************\nWelcome to curl downloader\n\nCtrl-C to terminate the program\n********************************\n'
+    os.system("mkdir ~/results")
+    output_file_name = expanduser("~") + "/results/" + socket.gethostname().replace("-","_") + "_" + sys.argv[3] + "_" + sys.argv[4] + "_" + datetime.datetime.now().strftime("%m%d%H%M")+".txt"
+    
+    if(sys.argv[2] == '0'):
+        cmd = 'curl -o /dev/null --limit-rate 1000k %s 2>&1 | tee -a %s' % (sys.argv[1], output_file_name)
+    else:
+        cmd = 'curl -o /dev/null --limit-rate 1000k --socks localhost:1080 %s 2>&1 | tee -a %s' % (sys.argv[1], output_file_name)
+
+    num_tasks = 1 
+    while True:
+        try:
+            s = '%s Task : %d\n' % (datetime.datetime.now().strftime(("%Y-%m-%d %H:%M:%S")), num_tasks)
+            print s
+            with open(output_file_name,"a") as f:
+                f.writelines(s)
+            p = Popen(cmd, shell=True)
+            p.communicate()
+            num_tasks += 1
+        except KeyboardInterrupt:
+            input = raw_input('\n\nTerminate the subprocess and exit?(y to exit, n to restart subprocess):')
+            if input == 'y':
+                p.terminate()
+                os._exit(-1)
+
+#cat 
+
