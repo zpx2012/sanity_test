0c9d394dabcd0ffdc8319c1dfff1b7007be27c84
zpx2012 <zhupengxiong@yeah.net>
2018-08-13 01:52:40 -0700

    add sleeper

--- ecs_top_alexa_0813.sh
+++ ecs_top_alexa_0813.sh
@@ -2,6 +2,7 @@ screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'https://dms.l
 screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'https://m.media-amazon.com/images/G/01/IMDb/design/prototypes/raven/allornothing/allblacks.mp4' 0 clean amazon;exec bash"
 screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'https://video-http.media-imdb.com/MV5BYThkNmNlOWQtNDA3My00MjgzLWI2ODktNWQxODcwOGZlOGVkXkExMV5BbXA0XkFpbWRiLWV0cy10cmFuc2NvZGU@.mp4?Expires=1534142129&Signature=c7tkEzp4GJsz-fKhfLqDNFvCEOuuwOoOaBlC-jZWAAikDwQbLuIj5IvkbDB-uvK6X2JPrtCr2okCeEW1ezWYJRbNEMcfhZiRcfZvyJ9aonPOAS6n47Xmk32eTHzza9Vp~UBz6plcXOdCJ0835CC-Tb6AxJZK10C3~ISmcfXkKVs_&Key-Pair-Id=APKAILW5I44IHKUN2DYA' 0 clean imdb;exec bash"
 screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'https://github.com/multipath-tcp/mptcp/archive/mptcp_v0.94.zip' 0 clean github;exec bash"
+traceroute -A dms.licdn.com > /dev/null
 traceroute -A dms.licdn.com > ~/sanity_test_results/tr_licdn.log
 traceroute -A upload.wikimedia.org > ~/sanity_test_results/tr_wiki.log
 traceroute -A m.media-amazon.com > ~/sanity_test_results/tr_amazon.log
