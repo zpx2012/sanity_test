1916b16ad4eebcb1fde80739321c396a3b143dd8
zpx2012 <zhupengxiong@yeah.net>
2018-08-17 23:46:08 -0700

    fix

--- iperf3_install.sh
+++ iperf3_install.sh
@@ -1,4 +1,6 @@
-sudo add-apt-repository -y "ppa:patrickdk/general-lucid"
-sudo apt-get -y update
-sudo apt-get -y install iperf3
-sudo iperf3 -s -4 -b 1m --cport 80 
+sudo apt-get -y remove iperf3 libiperf0
+wget https://iperf.fr/download/ubuntu/libiperf0_3.1.3-1_amd64.deb
+wget https://iperf.fr/download/ubuntu/iperf3_3.1.3-1_amd64.deb
+sudo dpkg -i libiperf0_3.1.3-1_amd64.deb iperf3_3.1.3-1_amd64.deb
+rm libiperf0_3.1.3-1_amd64.deb iperf3_3.1.3-1_amd64.deb
+sudo iperf3 -s -4  
