139c34fe7e0dce8eb0a787cd60fc1494f931f543
zpx2012 <zhupengxiong@yeah.net>
2018-08-19 22:27:57 -0700

    add -t 5

--- scripts/0819_iperf3_client.sh
+++ scripts/0819_iperf3_client.sh
@@ -1,1 +1,1 @@
-screen -dmS test bash -c 'iperf3 -c 169.235.31.181 -p 80 -b 1M -f k -4 --logfile iperf3.log;exec bash' 
+screen -dmS test bash -c 'iperf3 -c 169.235.31.181 -p 80 -b 1M -f k -t 5 -4 --logfile iperf3.log;exec bash' 
