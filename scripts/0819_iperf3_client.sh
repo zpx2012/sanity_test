efd5d17d1a626df282d932e3ccc85ee9f6f89626
zpx2012 <zhupengxiong@yeah.net>
2018-08-19 22:50:21 -0700

    no screen in iperf3_client.sh

--- scripts/0819_iperf3_client.sh
+++ scripts/0819_iperf3_client.sh
@@ -1,1 +1,4 @@
-screen -dmS test bash -c 'while true do iperf3 -c 169.235.31.181 -p 80 -b 1M -f k -t 5 -4 --logfile ~/iperf3.log done' 
+while true 
+do 
+iperf3 -c 169.235.31.181 -p 80 -b 1M -f k -t 5 -4 --logfile ~/iperf3.log 
+done
