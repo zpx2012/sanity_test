f3a0b7083f140c779052bd5b23c7760575862824
zpx2012 <zhupengxiong@yeah.net>
2018-08-22 00:05:03 -0700

    version 1.1.0 add  -4v; specify ip address

--- curl_downloader.py
+++ curl_downloader.py
@@ -1,30 +1,37 @@
-import os,sys,time,datetime,socket,shlex
+import os,sys,time,datetime,socket,urlparse
 from subprocess import Popen, PIPE
 from time import sleep
 from os.path import expanduser
 
 if __name__ == '__main__':
     if len(sys.argv) < 5:
-        print("Usage:\n\tpython curl_downloader.py [URL] [Mode] [Tool] [Site]\n\nOptions:\n\tMode:0:regular, 1:proxy mode\n\tTool: ss, vpn, ssh, from which server\n\tSite: download source, 163 or mit or so")
+        print("Usage:\n\tpython curl_downloader.py [URL] [IP] [Tool] [Site]\n\nOptions:\n\tTool: ss, vpn, ssh, from which server\n\tSite: download source, 163 or mit or so")
         sys.exit(-1)
+    url = sys.argv[1]
+    ip = sys.argv[2]
 
     out_dir = expanduser('~/sanity_test_results/')
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    output_file_name = out_dir + socket.gethostname() + "_" + sys.argv[3] + "_" + sys.argv[4] + "_" + datetime.datetime.now().strftime("%m%d%H%M")+".txt"
+    output_file_name = out_dir + 'curl' + socket.gethostname() + "_" + sys.argv[3] + "_" + sys.argv[4] + "_" + datetime.datetime.now().strftime("%m%d%H%M")+".txt"
 
     decorator = '\n********************************\n'
-    print decorator + 'Curl Downloader 1.0.3\nCtrl-C to terminate the program' + decorator + '\n'
+    print decorator + 'Curl Downloader 1.1.0\nCtrl-C to terminate the program' + decorator + '\n'
 
-    if(sys.argv[2] == '0'):
-        cmd = 'curl -o /dev/null --limit-rate 1000k --speed-time 1800 -LJ \'%s\' 2>&1 | tee -a %s' % (sys.argv[1], output_file_name)
+    nonproxy_modes = ['clean','https']
+    if 'https' in url:
+        port = 443
     else:
-        cmd = 'curl -o /dev/null --limit-rate 1000k --speed-time 1800 -LJ --socks localhost:1080 \'%s\' 2>&1 | tee -a %s' % (sys.argv[1], output_file_name)
+        port = 80
+    if sys.argv[3] in nonproxy_modes:
+        cmd = 'curl -o /dev/null --limit-rate 1000k --speed-time 1800 -LJv4 --resolve \'%s:%d:%s\' \'%s\' 2>&1 | tee -a %s' % (urlparse.urlparse(url), port, ip, url, output_file_name)
+    else:
+        cmd = 'curl -o /dev/null --limit-rate 1000k --speed-time 1800 -LJ --socks localhost:1080 \'%s\' 2>&1 | tee -a %s' % (url, output_file_name)
 
     num_tasks = 1 
     while True:
         with open(output_file_name,"a") as f:
-            s = '%s Task : %d\n URL:%s\n Method:%s' % (datetime.datetime.now().strftime(("%Y-%m-%d %H:%M:%S")), num_tasks, sys.argv[1], sys.argv[3])
+            s = '%s Task : %d\n URL:%s\n Method:%s' % (datetime.datetime.now().strftime(("%Y-%m-%d %H:%M:%S")), num_tasks, url, sys.argv[3])
             print s
             f.writelines(s)
         try:
---
+++ scripts/0821_top_alexa_vultr.sh
@@ -0,0 +1,22 @@
+kill -9 $(pgrep screen)
+screen -wipe
+screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'http://upload.wikimedia.org/wikipedia/commons/a/a8/LOC_Main_Reading_Room_Highsmith.jpg' '208.80.153.240' https wiki_highsmith;exec bash"
+screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'https://github.com/multipath-tcp/mptcp/archive/mptcp_v0.94.zip' '192.30.255.112' https github;exec bash"
+screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'https://m.media-amazon.com/images/G/01/IMDb/design/prototypes/raven/allornothing/allblacks.mp4' '23.38.126.22' https amazon;exec bash"
+
+screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'https://dms.licdn.com/playback/C4E00AQEOf4NNZp_DSA/23033a33657347f2a6f2a1140146d656/feedshare-mp4_3300/1488578169071-zmy00q?e=1535007600&v=beta&t=y5405te03Whmkw5VVtAcAOxXytB3Gi0niTMQJwHYVEQ' '23.38.127.219' https linkedin;exec bash"
+
+screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'https://video-http.media-imdb.com/MV5BY2FmN2I2NGQtMzI2NS00YzdiLTk2Y2YtOWIyZTEzZmJhYzg5XkExMV5BbXA0XkFpbWRiLWV0cy10cmFuc2NvZGU@.mp4?Expires=1535007546&Signature=xREp~NQN~YzLBIHoE6bGHB6cnZtidF0NMRE1ZE7NX67DWsW2gvK1D~W22D3mSFv4pxq6PvhoBQ8nABxwDttt4dSRyKUAzGBNpF5Gh8E8LKZ6RCNr63h044XGqhKaYUExui~SIP7xutsJK4env1JWB6dZkM-~AFlKVmk8g1LNZ3I_&Key-Pair-Id=APKAILW5I44IHKUN2DYA' '54.192.232.51' https imdb;exec bash"
+
+screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 'http://mirror.math.princeton.edu/pub/ubuntu-iso/ubuntu-core/16/ubuntu-core-16-pi2.img.xz' '128.112.18.21' 'clean' 'princeton'"
+
+screen -dmS test bash -c "python ~/sanity_test/curl_downloader.py 
+'https://mirror.tuna.tsinghua.edu.cn/ubuntu-releases/ubuntu-core/16/ubuntu-core-16-pi2.img.xz' '101.6.8.193' clean tsinghua"
+
+traceroute -A dms.licdn.com > /dev/null
+traceroute -A dms.licdn.com > ~/sanity_test_results/tr_licdn.log
+traceroute -A upload.wikimedia.org > ~/sanity_test_results/tr_wiki.log
+traceroute -A m.media-amazon.com > ~/sanity_test_results/tr_amazon.log
+traceroute -A video-http.media-imdb.com > ~/sanity_test_results/tr_imdb.log
+traceroute -A github.com > ~/sanity_test_results/tr_github.log
+screen -dmS test bash -c "python ~/sanity_test/mtr_runner.py '192.30.255.112' github; exec bash"
