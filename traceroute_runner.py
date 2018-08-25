232912816bb55a71c05884f8fa95afa46a11ab3c
zpx2012 <zhupengxiong@yeah.net>
2018-08-25 13:20:16 -0700

    new file:   top_china_10.csv 	new file:   traceroute_runner.py

---
+++ traceroute_runner.py
@@ -0,0 +1,16 @@
+import os,sys,time,datetime,socket,urlparse,threading,csv
+from subprocess import Popen, PIPE
+from time import sleep
+from os.path import expanduser
+
+if __name__ == '__main__':
+    
+    infile_name = sys.argv[1]
+    
+    if not os.path.isfile(infile_name):
+        print 'File does not exist.'
+    else:
+        with open(infile_name,'rb') as f: 
+            for line in list(csv.reader(f)):
+                output_file_name = '~/sanity_test_results/tr_' + socket.gethostname() + "_" + line[0] + "_" + datetime.datetime.now().strftime("%m%d%H%M")+".txt"
+                os.system('traceroute -A {} > {}'.format(line[1],output_file_name))
