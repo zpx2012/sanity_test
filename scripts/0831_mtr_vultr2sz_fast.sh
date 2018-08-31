8046b822403188463e4dcb3ffea5360798c35651
zpx2012 <zhupengxiong@yeah.net>
2018-08-31 01:14:59 -0700

    fix

--- scripts/0831_mtr_vultr2sz_fast.sh
+++ scripts/0831_mtr_vultr2sz_fast.sh
@@ -1,4 +1,4 @@
-startime = $(date +"%m%d%H%M")
+startime=$(date +"%m%d%H%M")
 while true
 do 
 sudo ~/mtr-modified/mtr -zwnr4T --port 80 -i 0.01 -c 1000 39.108.98.242 2>&1 | tee -a ~/sanity_test_results/mtr_new_$(hostname)2sz_tcp_0.01_1000_$startime.txt;
