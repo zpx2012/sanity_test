28d1129c4e24c1b6b8ec158157869622f71d664c
zpx2012 <zhupengxiong@yeah.net>
2018-09-01 00:10:11 -0700

    add 0901.sh

---
+++ scripts/0901_mtr_aliyun2terran_recur.sh
@@ -0,0 +1,6 @@
+startime=$(date +"%m%d%H%M")
+while true
+do 
+sudo mtr -zwnr4T -c 60 169.235.31.181 2>&1 | tee -a ~/sanity_test_results/mtr_old_$(hostname)2terran_syn_1_10_$startime.txt
+done
+exec bash
---
+++ scripts/install_sanity_test.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get -y install git
+git clone https://github.com/zpx2012/sanity_test.git ~/sanity_test
+sh ~/sanity_test/install.sh
