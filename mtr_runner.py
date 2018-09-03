c0a009991873c7169ee2a02705714fb5c44348ba
zpx2012 <zhupengxiong@yeah.net>
2018-09-03 00:52:42 -0700

    mtr version: 1.3.0

--- mtr_runner.py
+++ mtr_runner.py
@@ -22,37 +22,39 @@ def run_cmd_log(cmd,outfile):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 5:
+    if len(sys.argv) < 1:
         print("Usage:\n\tpython mtr_runner.py [new/old] [firstTTL] [cycle_num] [in.csv]")
         # print("Usage:\n\tpython mtr_runner.py [URL] [URL Nickname for Output]")
         sys.exit(-1)
 
     decorator = '\n********************************\n'
-    print decorator + 'Mtr Runner 1.2.0\nCtrl-C to terminate the program' + decorator + '\n'
+    print decorator + 'Mtr Runner 1.3.0\nCtrl-C to terminate the program' + decorator + '\n'
 
     out_dir = expanduser('~/sanity_test_results/')
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    output_filename_dict = {} 
+    output_filename_list = []
 
     version_dict = {'old':'mtr','new':'~/mtr-modified/mtr'}
-    base_cmd = 'sudo {} -zwnr4T -i {} -c {} --port 80 %s'.format(version_dict[sys.argv[1]],sys.argv[2],sys.argv[3])
+    base_cmd = 'sudo %s -zwnr4%s -i %s -c %s -f %s --port 80 %s 2>&1 | tee -a %s'#.format(version_dict[sys.argv[1]],sys.argv[2],sys.argv[3])
 
-    infile_name = sys.argv[4]
+    infile_name = sys.argv[1]
     
     if not os.path.isfile(infile_name):
         print 'File does not exist.'
     else:
         with open(infile_name,'rb') as f:         
             domain_ip_list = list(csv.reader(f))
-        for line in domain_ip_list:
-            output_filename_dict[line[0]] = out_dir + "mtr_" + sys.argv[1] + '_' + socket.gethostname() + "2" + line[0] + "_" + sys.argv[2] + '_' + sys.argv[3] +'_'+ datetime.datetime.now().strftime("%m%d%H%M")+".txt"
+        for i,line in enumerate(domain_ip_list):
+            output_filename_list.append(out_dir + "mtr_" + line[0] + '_' + socket.gethostname() + "2" + line[6] + '_' + line[7]+'_'+line[2] + '_' + line[3] +'_'+ datetime.datetime.now().strftime("%m%d%H%M")+".txt")
         num_tasks = 1 
-        while True:
-                for line in domain_ip_list:
-                    cmd = base_cmd % (line[1])
+        while num_tasks < 5:
+                for i,line in enumerate(domain_ip_list):
+                    cmd = base_cmd % (version_dict[line[0]],line[1],line[2],line[3],line[4],line[5],output_filename_list[i])
                     print cmd
-                    run_cmd_log(cmd,output_filename_dict[line[0]])
+                    os.system('echo %s >> %s'%(cmd,output_filename_list[i]))
+                    os.system(cmd)
+                    # run_cmd_log(cmd,output_filename_dict[line[0]])
                     num_tasks += 1
 
 
