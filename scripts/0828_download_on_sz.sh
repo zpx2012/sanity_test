f3fb0352b406178efa779d3889a62be01ff9c8b3
zpx2012 <zhupengxiong@yeah.net>
2018-09-21 21:29:09 -0700

    for mtr vultr2sz 0921

--- scripts/0828_download_on_sz.sh
+++ scripts/0828_download_on_sz.sh
@@ -1,6 +1,6 @@
-screen -dmS 0828_vultr_dl python ~/sanity_test/curl_downloader.py 'http://95.179.143.143/ubuntu-core-16-pi2.img.xz' '95.179.143.143' clean amsterdam-vultr 1
-screen -dmS 0828_vultr_dl python ~/sanity_test/curl_downloader.py 'http://95.179.167.194/ubuntu-core-16-pi2.img.xz' '95.179.167.194' clean frankfurt-vultr 1
-screen -dmS 0828_vultr_dl python ~/sanity_test/curl_downloader.py 'http://45.76.117.159/ubuntu-core-16-pi2.img.xz' '45.76.117.159' clean sydney-vultr 1
-screen -dmS 0828_vultr_dl python ~/sanity_test/curl_downloader.py 'http://199.247.8.100/ubuntu-core-16-pi2.img.xz' '199.247.8.100' clean paris-vultr 1
-screen -dmS 0828_vultr_dl python ~/sanity_test/curl_downloader.py 'http://108.160.139.201/ubuntu-core-16-pi2.img.xz' '108.160.139.201' clean tokyo-vultr 1
-screen -dmS 0828_vultr_dl python ~/sanity_test/curl_downloader.py 'http://149.28.153.244/ubuntu-core-16-pi2.img.xz' '149.28.153.244' clean singapore-vultr 1
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://95.179.143.143/ubuntu-core-16-pi2.img.xz' '95.179.143.143' clean amsterdam-vultr 0 400k
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://95.179.167.194/ubuntu-core-16-pi2.img.xz' '95.179.167.194' clean frankfurt-vultr 0 400k
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://45.76.117.159/ubuntu-core-16-pi2.img.xz' '45.76.117.159' clean sydney-vultr 0 400k
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://199.247.8.100/ubuntu-core-16-pi2.img.xz' '199.247.8.100' clean paris-vultr 0 400k
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://108.160.139.201/ubuntu-core-16-pi2.img.xz' '108.160.139.201' clean tokyo-vultr 0 400k
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://149.28.153.244/ubuntu-core-16-pi2.img.xz' '149.28.153.244' clean singapore-vultr 0 400k
