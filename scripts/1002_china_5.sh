d9454a1fd3d4a877d0d50219f15ebc7171ca4727
zpx2012 <zhupengxiong@yeah.net>
2018-10-02 12:16:55 -0700

    add for test 1002

---
+++ scripts/1002_china_5.sh
@@ -0,0 +1,10 @@
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'http://mirrors.163.com/ubuntu-releases/ubuntu-core/16/ubuntu-core-16-pi2.img.xz' '59.111.0.251' 163 1 500k 0
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'https://mirror.tuna.tsinghua.edu.cn/ubuntu-releases/ubuntu-core/16/ubuntu-core-16-pi2.img.xz' tsinghua 1 500k 0
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'http://ftp.sjtu.edu.cn/ubuntu-cd/ubuntu-core/16/ubuntu-core-16-pi2.img.xz' sjtu 1 500k 0
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'http://mirrors.huaweicloud.com/repository/ubuntu-cdimage/ubuntu-core/16/current/ubuntu-core-16-armhf%2Braspi3.img.xz' huawei 1 500k 0
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'http://scloud-dlsw.br.baidu.com/package/201703/bdbrowserSetup-7.6.110.3359-5646_11000002.exe' '58.211.137.1' baidu 1 500k 0
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'http://download.alicdn.com/wireless/taobao4android/latest/702757.apk' '140.205.16.80' taobao 1 500k 0
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'https://download.alicdn.com/wireless/tmallandroid/7.9.0-70521-14/10002119@tmallandroid_7.9.0.apk' '140.205.16.81' tmall 1 500k 0
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'https://dldir1.qq.com/qqfile/QQIntl/QQi_PC/QQIntl2.11.exe' '119.249.49.35' qq 1 500k 0
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'https://download.jr.jd.com/downapp/jrapp_jr464.apk' '61.174.55.1' jd 1 500k 0
+screen -dmS curl_cn python ~/sanity_test/curl_downloader.py 'http://dl.desktop.weibo.com/Weibo.4.5.3.37575common_wbupdate.exe' '122.225.38.198' weibo 1 500k 0
