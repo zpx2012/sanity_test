18b41f24bd7fcca5f72338d48e69e99b246e0b72
zpx2012 <zhupengxiong@yeah.net>
2018-10-05 00:43:02 -0700

    for 1005

---
+++ scripts/1005_sz_curl.sh
@@ -0,0 +1,7 @@
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://54.238.191.56/my.pcap' 54.238.191.56 okyo-aws 1 400k 0
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://54.169.60.179/my.pcap' 54.169.60.179 singapore-aws 1 400k 0
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://13.210.228.247/my.pcap' 13.210.228.247 sydney-aws 1 400k 0
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://144.202.55.149/my.pcap' 144.202.55.149 chicago-vultr 1 400k 0
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://45.77.5.73/my.pcap' 45.77.5.73 siliconvalley-vultr 1 400k 0
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://45.77.194.135/my.pcap' 45.77.194.135 miami-vultr 1 400k 0
+screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://45.32.221.43/my.pcap' 45.32.221.43 atlanta-vultr 1 400k 0
