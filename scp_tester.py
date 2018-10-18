a35bbb5b5ddb49b6e7d9064f0aa799a4d8abecaa
zpx2012 <zhupengxiong@yeah.net>
2018-10-18 05:02:51 -0700

    Changes to be committed: 	new file:   scp_tester.py 	new file:   scripts/scp_terran.csv 	new file:   scripts/scp_test_foreign.csv

---
+++ scp_tester.py
@@ -0,0 +1,39 @@
+import os,sys,time,datetime,socket,urlparse,threading,csv
+from subprocess import Popen, PIPE
+from time import sleep
+from os.path import expanduser
+from utils import run_cmd_wtimer
+from random import randint
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage:\n\tpython curl_downloader.py [URL] [IP] [Tool] [Site]\n\nOptions:\n\tTool: ss, vpn, ssh, from which server\n\tSite: download source, 163 or mit or so")
+        sys.exit(-1)
+
+    decorator = '\n********************************\n'
+    print decorator + 'scp tester 0.0.1\nCtrl-C to terminate the program' + decorator + '\n'
+    
+    out_dir = expanduser('~/sanity_test_results/')
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    output_filename_list = []
+
+    infile_name = sys.argv[1]
+    base_cmd = 'script -q -c "scp -i %s ~/my.pcap %s@%s:/dev/null" > %s'
+    
+    if not os.path.isfile(infile_name):
+        print 'File does not exist.'
+    else:
+        with open(infile_name,'rb') as f:         
+            domain_ip_list = list(csv.reader(f))
+        for i,line in enumerate(domain_ip_list):
+            output_filename_list.append(out_dir + '_'.join(['scp_upload',socket.gethostname()+'2'+line[3],datetime.datetime.utcnow().strftime('%m%d%H%Mutc')]) +'.txt')
+        num_tasks = 1 
+        while True:
+            for i,line in enumerate(domain_ip_list):
+                cmd = base_cmd % (line[0],line[1],line[2],output_filename_list[i])
+                with open(output_filename_list[i],'a') as f:
+                    f.writelines('\n%s Task : %d\n %s' % (datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S +0000'), num_tasks, cmd))
+                run_cmd_wtimer(cmd,10)
+                #visit_cn_websites(8)
+                num_tasks += 1
