4ce81317087cb07799a10f7e9c19fd59e3011e1d
zpx2012 <zhupengxiong@yeah.net>
2018-10-19 02:05:53 -0700

    fix

--- sched.py
+++ sched.py
@@ -5,18 +5,18 @@ from utils import run_cmd_shell_wtimer,run_cmd_wtimer
 def client_tcpdump():
     print('client_tcpdump: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     run_cmd_shell_wtimer('sudo tcpdump -w ~/packet_trace/hhht_http_client_calculate_loss_rate_%s.pcap -i eth0 -n host 169.235.31.181 ' % datetime.datetime.utcnow().strftime('%m%d%H%Mutc'),800)
-    print('client_tcpdump '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
+    print('client_tcpdump: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
 
 def server_tcpdump():
-    print('server_tcpdump '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
+    print('server_tcpdump: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     run_cmd_shell_wtimer('sudo tcpdump -w ~/packet_trace/terran_http_server_calculate_loss_rate_%s.pcap -i eth1 -n host 39.104.139.16 ' % datetime.datetime.utcnow().strftime('%m%d%H%Mutc'),800)
-    print('server_tcpdump '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
+    print('server_tcpdump: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
 
 def client_curl():
-    print('client_curl '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
+    print('client_curl: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     time.sleep(20)
     run_cmd_wtimer('curl -o /dev/null --limit-rate 750k --speed-time 120 -LJv4k http://169.235.31.181/my.pcap',600)
-    print('client_curl '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
+    print('client_curl: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
 
 if __name__ == '__main__':
     side = sys.argv[1]
