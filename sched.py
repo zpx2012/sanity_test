64a9b855a5d6acfbea6ad3c12e5c506246b7d22d
zpx2012 <zhupengxiong@yeah.net>
2018-10-21 23:39:06 -0700

    modified:   sched.py

--- sched.py
+++ sched.py
@@ -6,10 +6,10 @@ def tshark(dir,filename):
     cmd = 'cd %s;f=%s;tshark -r $f -Tfields -o tcp.relative_sequence_numbers:FALSE -e ip.id -e tcp.srcport -e tcp.dstport -e tcp.seq -e tcp.ack -e tcp.options.timestamp.tsecr -e tcp.options.timestamp.tsval > ${f%%.*}_1.txt;sort ${f%%.*}_1.txt > ${f%%.*}.tshark;rm ${f%%.*}_1.txt' % (dir,filename)
     run_cmd_shell(cmd)
 
-def tcpdump_tshark(out_dir,role,remote_hostname,remote_ip):
+def tcpdump_tshark(out_dir,interface,remote_ip,remote_hostname,role):
     print('tcpdump_tshark: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     out_filename = 'loss_%s_%s_%s_http_%s.pcap' % (socket.gethostname(),role,remote_hostname,datetime.datetime.utcnow().strftime('%m%d%H%Mutc'))
-    run_cmd_shell_wtimer('sudo tcpdump -w %s -i eth0 -n host %s' % (os.path.join(out_dir,out_filename),remote_ip),700)
+    run_cmd_shell_wtimer('sudo tcpdump -w %s -i %s -n host %s' % (os.path.join(out_dir,out_filename),remote_ip),700)
     tshark(out_dir,out_filename)    
     print('tcpdump_tshark: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')+'\n')
 
@@ -21,9 +21,10 @@ def client_curl():
 
 if __name__ == '__main__':
     side = sys.argv[1]
-    role = sys.argv[2]
-    rem_hn = sys.argv[3]
-    rem_ip = sys.argv[4]
+    intf = sys.argv[2]
+    rem_ip = sys.argv[3]
+    rem_hn = sys.argv[4]
+    role = sys.argv[5]
 
     out_dir = os.path.expanduser('~/packet_trace/sched_loss_') + datetime.datetime.utcnow().strftime('%m%d%H%Mutc')
     if not os.path.exists(out_dir):
@@ -33,7 +34,7 @@ if __name__ == '__main__':
     start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
     end   = start + datetime.timedelta(days=1)
     # Schedule job_function to be called every two hours     
-    sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,role,rem_hm,rem_ip],hours=1,start_date=start,end_date=end)
+    sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hm,role],hours=1,start_date=start,end_date=end)
     if side == 'client':    
         sched.add_job(client_curl, 'interval', hours=1,start_date=start+datetime.timedelta(seconds=10),end_date=end)
     
