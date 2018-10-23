fa093a91ca965c34a1a4c44f8c198b32cdd0185b
zpx2012 <zhupengxiong@yeah.net>
2018-10-23 00:54:40 -0700

    schedule sender

--- sched.py
+++ sched.py
@@ -19,27 +19,35 @@ def client_curl():
     run_cmd_wtimer('script -aqf -c \'while true;do curl -o /dev/null --limit-rate 500k --speed-time 120 -LJv4k http://169.235.31.181/my.pcap; sleep 10;done\' %s/curl_terran_loss_test_%s.txt'%(out_dir,datetime.datetime.utcnow().strftime('%m%d%H%Mutc')),600)
     print('client_curl: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')+'\n')
 
+def client_sender():
+    os.system('cd ~/sanity_test;./sender_client 169.235.31.181')
+
+def server_sender():
+    os.system('cd ~/sanity_test;sudo ./sender_server')
+
 if __name__ == '__main__':
     intf = sys.argv[1]
     rem_ip = sys.argv[2]
     rem_hn = sys.argv[3]
     role = sys.argv[4]
 
-    out_dir = os.path.expanduser('~/packet_trace/sched_loss_') + datetime.datetime.utcnow().strftime('%m%d%H%Mutc')
+    out_dir = os.path.expanduser('~/packet_trace/sched_sender_') + datetime.datetime.utcnow().strftime('%m%d%H%Mutc')
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
     sched = BlockingScheduler(timezone=pytz.utc)
-    start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
-    end   = start + datetime.timedelta(days=1)
+    start = datetime.datetime.strptime('2018-10-23 14:00:00','%Y-%m-%d %H:%M:%S')    
+    # start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
+    end   = start + datetime.timedelta(hours=2)
     # Schedule job_function to be called every two hours     
     sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],hours=1,start_date=start,end_date=end)
     if role == 'client':    
-        sched.add_job(client_curl, 'interval', hours=1,start_date=start+datetime.timedelta(seconds=10),end_date=end)
-    
+        sched.add_job(client_sender, 'interval', hours=1,start_date=start+datetime.timedelta(seconds=20),end_date=end)
+    elif role == 'server':
+        sched.add_job(client_sender, 'interval', hours=1,start_date=start+datetime.timedelta(seconds=10),end_date=end)
     sched.start()
 
-    # start = datetime.datetime.strptime('2018-10-19 07:50:00','%Y-%m-%d %H:%M:%S')
+
     # def server_tcpdump():
     # print('server_tcpdump: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     # run_cmd_shell_wtimer('sudo tcpdump -w %s/terran_http_server_calculate_loss_rate_%s.pcap -i eth1 -n host 39.104.139.16 ' % (out_dir,datetime.datetime.utcnow().strftime('%m%d%H%Mutc')),800)
