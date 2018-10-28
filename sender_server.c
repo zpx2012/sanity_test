168e2fa31d6b51e0a57444a9ff1ef65ebf05a2eb
zpx2012 <zhupengxiong@yeah.net>
2018-10-28 05:32:51 -0700

    fix

--- sender_server.c
+++ sender_server.c
@@ -382,7 +382,8 @@ int main(int argc , char *argv[])
 
     if(mode == 0){
  
-        float pkt_intvl_array[11] = {0.01,0.03,0.05,0.07,0.1,0.3,0.5,0.7,1,5,10};
+        // float pkt_intvl_array[11] = {0.01,0.03,0.05,0.07,0.1,0.3,0.5,0.7,1,5,10};
+        float pkt_intvl_array[11] = {0.01,0.1,1,5,10};
         int max_pkt_intvl_index = 11;
 
         int i;
