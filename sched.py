da3144fb2707c00b511b44d8c630425ff87ab5ef
zpx2012 <zhupengxiong@yeah.net>
2018-10-28 05:45:47 -0700

    fix

--- sched.py
+++ sched.py
@@ -20,7 +20,7 @@ def tshark_capture(out_dir,interface,remote_ip,remote_hostname,role):
 def tcpdump_tshark(out_dir,interface,remote_ip,remote_hostname,role):
     print('tcpdump_tshark: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     out_filename = 'loss_%s_%s_%s_http_%s.pcap' % (socket.gethostname(),role,remote_hostname,datetime.datetime.utcnow().strftime('%m%d%H%Mutc'))
-    run_cmd_wtimer('tcpdump -w %s -i %s -n host %s and tcp port 80' % (os.path.join(out_dir,out_filename),interface,remote_ip),10)
+    run_cmd_wtimer('tcpdump -w %s -i %s -n host %s and tcp port 80' % (os.path.join(out_dir,out_filename),interface,remote_ip),610)
     tshark(out_dir,out_filename)    
     print('tcpdump_tshark: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')+'\n')
 
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     seq = 0
     # start = datetime.datetime.strptime('2018-10-26 %s:00:00' % hour,'%Y-%m-%d %H:%M:%S') 
     start_str = '1026%s00' % hour   
-    start = datetime.datetime.utcnow() + datetime.timedelta(seconds=3)
+    start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
     end   = start + datetime.timedelta(hours=2)
     sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=610,start_date=start,end_date=end)
     # if role == 'client':    
