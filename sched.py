dcd7252129710ca0c3bd6a512cc807f31965613e
zpx2012 <zhupengxiong@yeah.net>
2018-10-28 05:05:36 -0700

    sched

--- sched.py
+++ sched.py
@@ -1,21 +1,26 @@
-import datetime,time,sys,pytz,os,socket
+import datetime,time,sys,pytz,os,socket,signal
 import subprocess as sp
 from apscheduler.schedulers.blocking import BlockingScheduler
 from utils import run_cmd_shell_wtimer,run_cmd_wtimer,run_cmd_shell
 
-def tshark(out_dir,interface,remote_ip,remote_hostname,role):
+def tshark(dir,filename):
+    cmd = 'cd %s;f=%s;tshark -r $f -Tfields -o tcp.relative_sequence_numbers:FALSE -e ip.id -e tcp.srcport -e tcp.dstport -e tcp.seq -e tcp.ack -e tcp.options.timestamp.tsecr -e tcp.options.timestamp.tsval > ${f%%.*}_1.txt;sort ${f%%.*}_1.txt > ${f%%.*}.tshark;rm ${f%%.*}_1.txt' % (dir,filename)
+    run_cmd_shell(cmd)
+
+
+def tshark_capture(out_dir,interface,remote_ip,remote_hostname,role):
     global seq
     print('tshark: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     out_filename = 'loss_%s_%s_%s_%d_%s.tshark' % (socket.gethostname(),role,remote_hostname,seq,datetime.datetime.utcnow().strftime('%m%d%H%Mutc'))
     tshark_cmd = 'sudo tshark -i %s -f \'host %s and tcp port %s\' -Tfields -o tcp.relative_sequence_numbers:FALSE -e ip.id -e tcp.srcport -e tcp.dstport -e tcp.seq -e tcp.ack -e tcp.options.timestamp.tsecr -e tcp.options.timestamp.tsval > %s'
-    run_cmd_shell_wtimer(tshark_cmd % (interface,remote_ip,80,os.path.join(out_dir,out_filename)),600)
+    run_cmd_shell_wtimer(tshark_cmd % (interface,remote_ip,80,os.path.join(out_dir,out_filename)),602)
     seq += 1
     print('tshark: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')+'\n')
 
 def tcpdump_tshark(out_dir,interface,remote_ip,remote_hostname,role):
     print('tcpdump_tshark: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     out_filename = 'loss_%s_%s_%s_http_%s.pcap' % (socket.gethostname(),role,remote_hostname,datetime.datetime.utcnow().strftime('%m%d%H%Mutc'))
-    run_cmd_shell_wtimer('sudo tcpdump -w %s -i %s -n host %s' % (os.path.join(out_dir,out_filename),interface,remote_ip),7200)
+    run_cmd_shell_wtimer('sudo tcpdump -w %s -i %s -n host %s and tcp port 80' % (os.path.join(out_dir,out_filename),interface,remote_ip),7200)
     tshark(out_dir,out_filename)    
     print('tcpdump_tshark: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')+'\n')
 
@@ -27,7 +32,7 @@ def loss_test(out_dir,interface,remote_ip,remote_hostname,role,seq):
     p_td = sp.Popen(tshark_cmd % (os.path.join(out_dir,out_filename),interface,remote_ip),shell=True, preexec_fn=os.setpgrp)
     #do something
 
-    os.killpg(os.getpgid(p.pid), signal.SIGTERM)
+    os.killpg(os.getpgid(p_td.pid), signal.SIGTERM)
 
 def client_curl():
     global start_str
@@ -39,7 +44,7 @@ def client_sender():
     os.system('cd ~/sanity_test;./sender_client 169.235.31.181')
 
 def server_sender():
-    os.system('cd ~/sanity_test;sudo ./sender_server')
+    os.system('cd ~/sanity_test;sudo ./sender_server 500 0')
 
 if __name__ == '__main__':
     intf = sys.argv[1]
@@ -58,14 +63,14 @@ if __name__ == '__main__':
 
     sched = BlockingScheduler(timezone=pytz.utc)
     seq = 0
-    start = datetime.datetime.strptime('2018-10-26 %s:00:00' % hour,'%Y-%m-%d %H:%M:%S') 
+    # start = datetime.datetime.strptime('2018-10-26 %s:00:00' % hour,'%Y-%m-%d %H:%M:%S') 
     start_str = '1026%s00' % hour   
-    #start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
+    start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
     end   = start + datetime.timedelta(hours=2)
-    sched.add_job(tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=602,start_date=start,end_date=end)
+    sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=610,start_date=start,end_date=end)
     if role == 'client':    
-        sched.add_job(client_sender, 'date', run_date=start)
-        sched.add_job(client_curl, 'date', run_date=start)
+        sched.add_job(client_sender, 'date', run_date=start+datetime.timedelta(seconds=1))
+        # sched.add_job(client_curl, 'date', run_date=start)
     elif role == 'server':
         sched.add_job(server_sender, 'date', run_date=start)
 
