c440d1e51c37427960b2ff0d554f906391fc0dcc
zpx2012 <zhupengxiong@yeah.net>
2018-10-28 08:21:05 -0700

    fix

--- sched.py
+++ sched.py
@@ -70,8 +70,8 @@ if __name__ == '__main__':
     # start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
     end   = start + datetime.timedelta(minutes=119)
     intvl = 3300
-    sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=620,start_date=start,end_date=start+datetime.timedelta(hour=1))
-    sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=620,start_date=start+datetime.timedelta(seconds=intvl),end_date=start+datetime.timedelta(hour=2))
+    sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=620,start_date=start,end_date=start+datetime.timedelta(housr=1))
+    sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=620,start_date=start+datetime.timedelta(seconds=intvl),end_date=start+datetime.timedelta(hours=2))
     if role == 'client':    
         sched.add_job(client_sender, 'interval', seconds=intvl, start_date=start+datetime.timedelta(seconds=1), end_date=end)
         # sched.add_job(client_curl, 'date', run_date=start)
