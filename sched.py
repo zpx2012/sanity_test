6f4168962ccb001b13c9e7dfb7ce134f461740c2
zpx2012 <zhupengxiong@yeah.net>
2018-10-28 05:46:12 -0700

    fix

--- sched.py
+++ sched.py
@@ -68,11 +68,11 @@ if __name__ == '__main__':
     start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
     end   = start + datetime.timedelta(hours=2)
     sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=610,start_date=start,end_date=end)
-    # if role == 'client':    
-    #     sched.add_job(client_sender, 'date', run_date=start+datetime.timedelta(seconds=1))
-    #     # sched.add_job(client_curl, 'date', run_date=start)
-    # elif role == 'server':
-    #     sched.add_job(server_sender, 'date', run_date=start)
+    if role == 'client':    
+        sched.add_job(client_sender, 'date', run_date=start+datetime.timedelta(seconds=1))
+        # sched.add_job(client_curl, 'date', run_date=start)
+    elif role == 'server':
+        sched.add_job(server_sender, 'date', run_date=start)
 
     #Schedule job_function to be called every two hours     
    
