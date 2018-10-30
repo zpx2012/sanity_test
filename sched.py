8651a7682253a4b19362629f34ad44c1f618f0d5
zpx2012 <zhupengxiong@yeah.net>
2018-10-30 02:10:12 -0700

    fix

--- sched.py
+++ sched.py
@@ -104,9 +104,9 @@ if __name__ == '__main__':
 
     sched = BlockingScheduler(timezone=pytz.utc)
     seq = 0
-#    start = datetime.datetime.strptime('2018-10-28 %s:30:00' % hour,'%Y-%m-%d %H:%M:%S') 
+    start = datetime.datetime.strptime('2018-10-30 09:11:00' % hour,'%Y-%m-%d %H:%M:%S') 
     # start_str = '1028%s00' % hour   
-    start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
+    # start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
     end   = start + datetime.timedelta(minutes=119)
     intvl = 3300
 #    sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=620,start_date=start+datetime.timedelta(seconds=intvl),end_date=start+datetime.timedelta(hours=2))
