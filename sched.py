4f91aafe85864489288d96a9ef96c4959e0b2deb
zpx2012 <zhupengxiong@yeah.net>
2018-11-04 01:37:55 -0700

    fix

--- sched.py
+++ sched.py
@@ -13,6 +13,7 @@ def tshark_capture(out_dir,interface,remote_ip,remote_hostname,port,role,duratio
     print('tshark: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     out_filename = 'loss_%s_%s_%s_%02d_%s.tshark' % (socket.gethostname(),role,remote_hostname,seq,datetime.datetime.utcnow().strftime('%m%d%H%Mutc'))
     tshark_cmd = 'tshark -i %s -f \'host %s and tcp port %s\' -Tfields -o tcp.relative_sequence_numbers:FALSE -e ip.id -e tcp.srcport -e tcp.dstport -e tcp.seq -e tcp.ack -e tcp.options.timestamp.tsecr -e tcp.options.timestamp.tsval' % (interface,remote_ip,port)
+    output = ''
     try:
         output = sp.check_output(shlex.split(tshark_cmd) ,timeout=duration)
     except sp.TimeoutExpired:
