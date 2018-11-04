b0af8cf505d72bf363db2c5385962ef82dca32a7
zpx2012 <zhupengxiong@yeah.net>
2018-11-04 01:05:35 -0700

    fix

--- sched.py
+++ sched.py
@@ -12,7 +12,7 @@ def tshark_capture(out_dir,interface,remote_ip,remote_hostname,port,role,duratio
     global seq
     print('tshark: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     out_filename = 'loss_%s_%s_%s_%02d_%s.tshark' % (socket.gethostname(),role,remote_hostname,seq,datetime.datetime.utcnow().strftime('%m%d%H%Mutc'))
-    tshark_cmd = 'tshark -i %s host %s and tcp port %s -Tfields -o tcp.relative_sequence_numbers:FALSE -e ip.id -e tcp.srcport -e tcp.dstport -e tcp.seq -e tcp.ack -e tcp.options.timestamp.tsecr -e tcp.options.timestamp.tsval > %s'
+    tshark_cmd = 'tshark -i %s -f \'host %s and tcp port %s\' -Tfields -o tcp.relative_sequence_numbers:FALSE -e ip.id -e tcp.srcport -e tcp.dstport -e tcp.seq -e tcp.ack -e tcp.options.timestamp.tsecr -e tcp.options.timestamp.tsval > %s'
     run_cmd_wtimer(tshark_cmd % (interface,remote_ip,port,os.path.join(out_dir,out_filename)),duration)
     print('tshark: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')+'\n')
 
@@ -130,9 +130,9 @@ if __name__ == '__main__':
 
     sched = BlockingScheduler(timezone=pytz.utc)
     seq = 0
-    start = datetime.datetime.strptime('2018-11-04 07:%s:00' % minute,'%Y-%m-%d %H:%M:%S') 
+    # start = datetime.datetime.strptime('2018-11-04 07:%s:00' % minute,'%Y-%m-%d %H:%M:%S') 
     # start_str = '1028%s00' % hour   
-    # start = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
+    start = datetime.datetime.utcnow() + datetime.timedelta(seconds=3)
     end   = start + datetime.timedelta(days=1)
     # intvl = 3300
 #    sched.add_job(tcpdump_tshark, 'interval', args=[out_dir,intf,rem_ip,rem_hn,role],seconds=620,start_date=start+datetime.timedelta(seconds=intvl),end_date=start+datetime.timedelta(hours=2))
