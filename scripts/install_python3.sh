c0f2f985bbd73b842270d11eb9cfa8f6b36fc534
zpx2012 <zhupengxiong@yeah.net>
2018-11-08 02:28:50 -0800

    fix

--- scripts/install_python3.sh
+++ scripts/install_python3.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get -y --fix-missing install python3 python3-pip
+sudo apt-get -y --fix-missing install python3 python3-pip python3-setuptools
 sudo pip3 install apscheduler pytz 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install wireshark tshark
 echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
