86ec42f41bef2f63e8be5a4cc316af609765c0ff
zpx2012 <zhupengxiong@yeah.net>
2018-11-11 01:37:57 -0800

    fix

---
+++ scripts/1111_iperf3_rgl_cubic.sh
@@ -0,0 +1,5 @@
+while true 
+do 
+iperf3 -c 169.235.31.181 -p 20000 -b 1M -f k -t 5 -4VRd --logfile ~/iperf3_$(USER)_$(hostname)_terran_cubic_$(date -u +"%m%d%H%M").log 
+sleep 10
+done
--- scripts/install_endloss_capture.sh
+++ scripts/install_endloss_capture.sh
@@ -4,4 +4,5 @@ sudo pip3 install apscheduler pytz
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install wireshark tshark
 echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
 sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure wireshark-common
-
+inf=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
+sudo ethtool -K $inf tso off gso off gro off
