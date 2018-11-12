4081a706774bf1760ce6ef9cb00f385395940c84
zpx2012 <zhupengxiong@yeah.net>
2018-11-12 03:52:58 -0800

    fix

--- scripts/iperf3_client.sh
+++ scripts/iperf3_client.sh
@@ -4,9 +4,9 @@ while true
 do 
 if [ $3 == regular ]
 then
-iperf3 -c $1 -p $2 -b 1M -f K -t 10 -4Vd -C cubic --logfile ~/iperf3_$(hostname)_$3_$1_cubic_$startime.txt 
+iperf3 -c $1 -p $2 -b 1M -f K -t 10 -4V -C cubic --logfile ~/iperf3_$(hostname)_$3_$1_cubic_$startime.txt 
 else
-sudo iperf3 -c $1 -p $2 -b 1M -f K -t 10 -4Vd --logfile ~/iperf3_$(hostname)_$3_$1_$startime.txt
+sudo iperf3 -c $1 -p $2 -b 1M -f K -t 10 -4V --logfile ~/iperf3_$(hostname)_$3_$1_$startime.txt
 fi
 sleep 10
 done
