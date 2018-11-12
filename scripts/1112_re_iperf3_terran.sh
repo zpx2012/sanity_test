9cca74503f1e8c681218418eaf6a8f41cee01e54
zpx2012 <zhupengxiong@yeah.net>
2018-11-12 03:52:38 -0800

    fix

--- scripts/1112_re_iperf3_terran.sh
+++ scripts/1112_re_iperf3_terran.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 for ip in 39.104.139.16 39.108.98.242
 do
-screen -dmS re_iperf3 sh ~/sanity_test/scripts/iperf3_client.sh $ip 5201 regular
-screen -dmS re_iperf3 sh ~/sanity_test/scripts/iperf3_client.sh $ip 80 root
+screen -dmS re_iperf3 bash ~/sanity_test/scripts/iperf3_client.sh $ip 5201 regular
+screen -dmS re_iperf3 bash ~/sanity_test/scripts/iperf3_client.sh $ip 80 root
 done
--- scripts/iperf3_client.sh
+++ scripts/iperf3_client.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+startime=$(date -u +"%m%d%H%Mutc")
 while true 
 do 
-startime=$(date -u +"%m%d%H%Mutc")
 if [ $3 == regular ]
 then
-iperf3 -c $1 -p $2 -b 1M -f K -t 5 -4Vd -C cubic --logfile ~/iperf3_$(hostname)_$3_$1_cubic_$startime.txt 
+iperf3 -c $1 -p $2 -b 1M -f K -t 10 -4Vd -C cubic --logfile ~/iperf3_$(hostname)_$3_$1_cubic_$startime.txt 
 else
-sudo iperf3 -c $1 -p $2 -b 1M -f K -t 5 -4Vd --logfile ~/iperf3_$(hostname)_$3_$1_$startime.txt
+sudo iperf3 -c $1 -p $2 -b 1M -f K -t 10 -4Vd --logfile ~/iperf3_$(hostname)_$3_$1_$startime.txt
 fi
 sleep 10
 done
