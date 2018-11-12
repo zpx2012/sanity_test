ea4f9d19448d66352d9261e53455fa972bc265e3
zpx2012 <zhupengxiong@yeah.net>
2018-11-12 03:57:03 -0800

    fix

--- scripts/1112_re_iperf3_aliyun.sh
+++ scripts/1112_re_iperf3_aliyun.sh
@@ -1,4 +1,4 @@
 screen -dmS curl_vultr python ~/sanity_test/curl_downloader.py 'http://169.235.31.181/my.pcap' '169.235.31.181' clean terran 0 750k
 startime=$(date -u +"%m%d%H%Mutc")
 screen -dmS root_iperf iperf3 -s -p 80 -f K --logfile ~/iperf3_server_root_$(hostname)_terran_$startime.txt
-sudo -u regular bash -c 'script /dev/null;screen -dmS rgl_iperf iperf3 -s -f K --logfile ~/iperf3_server_regular_$(hostname)_terran_$startime.txt'
+sudo -u regular bash -c "script /dev/null;screen -dmS rgl_iperf iperf3 -s -f K --logfile ~/iperf3_server_regular_$(hostname)_terran_$startime.txt"
