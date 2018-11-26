d9b8689ea98b230b93a3d329dce6eb3958dd2799
zpx2012 <zhupengxiong@yeah.net>
2018-11-26 02:08:16 -0800

    fix

--- sched.py
+++ sched.py
@@ -129,7 +129,7 @@ def curl_vultr(line,output_file_name):
 def tcpdump_1116(out_dir,remote_ip,remote_hostname,port,duration):
     print('tcpdump_1116: start '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'))
     out_filename = 'loss_%s_%s_%d_%s.pcap' % (socket.gethostname(),remote_hostname,port,datetime.datetime.utcnow().strftime('%m%d%H%Mutc'))
-    run_cmd_wtimer('tcpdump -w %s -s 96 -i eth0 -n host %s and tcp port %d' % (os.path.join(out_dir,out_filename),remote_ip,port),duration)
+    run_cmd_wtimer('tcpdump -w %s -s 96 -i ens3 -n host %s and tcp port %d' % (os.path.join(out_dir,out_filename),remote_ip,port),duration)
     sp.call('ls -hl %s' % os.path.join(out_dir,out_filename),shell=True)
     print('tcpdump_1116: end '+datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')+'\n')
 
--- scripts/1125_ss_http_server.sh
+++ scripts/1125_ss_http_server.sh
@@ -1,3 +1,3 @@
-screen -dmS ss ssserver -c ~/shadowsocks/ss_server.json
+screen -dmS ss ssserver -c ~/sanity_test/shadowsocks/ss_server.json
 sleep 120
 screen -dmS sched python3 ~/sanity_test/sched.py
