b6017421f74803bbde24ae0a5e2f23021d16025d
zpx2012 <zhupengxiong@yeah.net>
2018-11-26 01:35:04 -0800

    git fix

--- scripts/1125_ss_http_client.sh
+++ scripts/1125_ss_http_client.sh
@@ -1,8 +1,9 @@
-cd ~/sanity_test
+cd ~/sanity_test/shadowsocks
 for f in shadowsocks/client*.json
 do
     screen -dmS ss_$f sslocal -c $f
 done
+cd ..
 screen -dmS nethog nethogs -t eth0 &> ~/sanity_test_results/nethogs_$(hostname)_$(date -u +"%m%d%H%M%Sutc").txt
 export IFS=","
 cat scripts/vultr.csv | while read hm ip port; do 
