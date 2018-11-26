00c1173544c7c7934d95dea9b306d20f0475e9e3
zpx2012 <zhupengxiong@yeah.net>
2018-11-26 01:48:11 -0800

    fix

--- scripts/1125_ss_http_client.sh
+++ scripts/1125_ss_http_client.sh
@@ -6,7 +6,7 @@ done
 cd ..
 screen -dmS nethog nethogs -t eth0 &> ~/sanity_test_results/nethogs_$(hostname)_$(date -u +"%m%d%H%M%Sutc").txt
 export IFS=","
-cat scripts/vultr.csv | while read hm ip port; do 
+cat scripts/ss_vultr.csv | while read hm ip port; do 
 screen -dmS curl_http_$hm python curl_downloader.py "http://$ip/my.pcap" $ip $hm 0 500k 0
 screen -dmS curl_ss_$hm python curl_downloader.py "http://$ip/my.pcap" $ip $hm 0 500k $port
 done
