db383ad00dab0efd183727665ecbd6821a135800
zpx2012 <zhupengxiong@yeah.net>
2018-12-05 02:28:11 -0800

    for 1204 russia

---
+++ scripts/1204_russia.sh
@@ -0,0 +1,2 @@
+screen -dmS curl_1 python3 ~/sanity_test/curl_poll.py ~/sanity_test/scripts/1204_russia_1.csv
+screen -dmS curl_2 python3 ~/sanity_test/curl_poll.py ~/sanity_test/scripts/1204_russia_2.csv
