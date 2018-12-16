9f4e63c00d119fc5a6d67364102b5e72db72484e
zpx2012 <zhupengxiong@yeah.net>
2018-12-16 04:12:22 -0800

    fix

--- scripts/1216.sh
+++ scripts/1216.sh
@@ -1,14 +1,14 @@
 stime=$(date -u +%m%d%H%M)
 mkdir ~/packet_trace/loss_$stime
 export IFS=","
-if [ $1 == s ]
+if [[ $1 == s ]]
 then
 cat ~/sanity_test/scripts/1125_aliyun.csv | while read hm ip port; do 
 screen -dmS ptr bash -c 'tfile=~/sanity_test_results/ptraceroute_'$hm_$(hostname)_$stime'_server.txt;while true;do date -u +"%Y-%m-%d %H:%M:%S %Z" >> $tfile;sudo paris-traceroute -Q -s 80 -d 33456 -p tcp '$ip' >> $tfile;done;exec bash'
 cd ~/packet_trace/loss_$stime
 screen -dmS td bash -c 'tcpdump -w tcpdump_'$hm_$(hostname)'_$(date -u +%m%d%H%M%S)utc_server.pcap -G 1 -s 96 -i ens3 -n host '$ip' and tcp port 80;exec bash'
 done
-elif [ $1 == c ]
+elif [[ $1 == c ]]
 then
 cat ~/sanity_test/scripts/1125_vultr.csv | while read hm ip port; do 
 screen -dmS ptr bash -c 'tfile=~/sanity_test_results/ptraceroute'$(hostname)_$hm_$stime'_client.txt;while true;do date -u +"%Y-%m-%d %H:%M:%S %Z" >> $tfile; paris-traceroute -Q -d 80 -p tcp '$ip' >> $tfile ;done;exec bash'
