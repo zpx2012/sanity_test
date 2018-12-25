0bd0c9f9292160871396a43b7ca242f0774d558e
zpx2012 <zhupengxiong@yeah.net>
2018-12-25 01:50:49 -0800

    fix

--- scripts/1223_mtr.sh
+++ scripts/1223_mtr.sh
@@ -1,3 +1,3 @@
 #ip=182.148.96.45
 ip=203.129.9.9
-creen -dmS mtr4809 sh ~/sanity_test/scripts/tmpl_mtr_icmp_1_100.sh $ip $ip-AS4809
+screen -dmS mtr4809 sh ~/sanity_test/scripts/tmpl_mtr_icmp_1_100.sh $ip $ip-AS4809
