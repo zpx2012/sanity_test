f33d60d7620ad66941164788db104d11b44b8a6d
zpx2012 <zhupengxiong@yeah.net>
2018-12-31 22:54:13 -0800

    fix

---
+++ ip_scan/port_scan.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    masscan/bin/masscan -p0-65535 --max-rate 100000 --output-format list $line >> "$2" 2>&1
+    echo >> "$2"
+done < "$1"
