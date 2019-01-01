982e50a9da0e2dcba4a5944c0a8725f574ac9264
zpx2012 <zhupengxiong@yeah.net>
2019-01-01 04:45:54 -0800

    fix

--- ip_scan/port_scan.sh
+++ ip_scan/port_scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo $line
-    ~/masscan/bin/masscan -p0-65535 --max-rate 1000 --oL ~/sanity_test_result/pscan_$(echo $line | cut -d'/' -f1).txt
+    ~/masscan/bin/masscan -p0-65535 --max-rate 1000 -oL ~/sanity_test_result/pscan_$(echo $line | cut -d'/' -f1).txt
 done < "$1"
