dc3c879ef9337e2bf9020d04afc0291f9c844c29
zpx2012 <zhupengxiong@yeah.net>
2019-01-07 04:55:23 -0800

    fix

--- ip_scan/synack_test.sh
+++ ip_scan/synack_test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 i=0
+stime=$(date -u +%m%d%H%M)
+mkdir ~/packet_trace/loss_$stime
 gcc ~/sanity_test/ip_scan/synack_client.c -o ~/sanity_test/ip_scan/synack_client
-for ip in 103.44.144.168 103.44.144.186 103.44.144.176 103.44.144.83 103.44.144.14 103.44.144.10 122.10.136.32 122.10.136.103 122.10.136.104 43.225.180.30 43.225.180.40 43.225.180.38 43.225.180.24 43.225.180.226 43.225.180.131
-do
-# echo "sudo tcpdump -w tcpdump_${ip}_$(hostname)_"'%m%d%H%M%S%z'"utc.pcap -G 60 -s 96 -i eth1 -n host $ip and tcp port 80;exec bash" > tmp$((++i)).sh
-# screen -dmS td_$ip bash tmp$i.sh
-screen -dmS cl_$ip bash -c "while true;do ./sanity_test/ip_scan/synack_client $ip 80;done"
-# screen -dmS tr_$ip bash -c "sudo traceroute -A -p 80 -P tcp -f 4 -m 25 $ip >> ~/sanity_test_results/tr_terran_${ip}_80.txt 2>&1" 
+cat $1 | while IFS='' read -r ip || [[ -n "$ip" ]]; do
+    sudo traceroute -A -p 80 -P tcp -f 4 -m 25 $ip >> ~/sanity_test_results/tr_terran_${ip}_80.txt 2>&1
+    rt=$(cat ~/sanity_test_results/tr_terran_${ip}_80.txt | grep 202.97)
+    if [ ! -z "$rt" -a "$rt" != " " ]; then
+        cd ~/packet_trace/loss_$stime
+        echo "sudo tcpdump -w tcpdump_${ip}_$(hostname)_"'%m%d%H%M%S%z'"utc.pcap -G 60 -s 96 -i eth1 -n host $ip and tcp port 80;exec bash" > tmp$((++i)).sh
+        screen -dmS td_$ip bash tmp$i.sh
+        screen -dmS cl_$ip bash -c "while true;do ~/sanity_test/ip_scan/synack_client $ip 80;done"
+        # screen -dmS tr_$ip bash -c "sudo traceroute -A -p 80 -P tcp -f 4 -m 25 $ip >> ~/sanity_test_results/tr_terran_${ip}_80.txt 2>&1" 
+    fi
 done
