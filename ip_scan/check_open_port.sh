8a40a421aea84cc60f45dd7bb693e5af58915ea1
zpx2012 <zhupengxiong@yeah.net>
2019-01-07 04:34:10 -0800

    fix

--- ip_scan/check_open_port.sh
+++ ip_scan/check_open_port.sh
@@ -3,7 +3,7 @@ for f in pscan_*;do
         # echo $open $tcp $port $ip $tm
         if [ ! -z "$ip" -a "$ip" != " " ]; then
             echo "nc -z -w -5 $ip $port >> co_$f 2>&1"
-            nc -z -w 5 $ip $port >> co_$f 2>&1
+            nc -z -v -w 5 $ip $port >> co_$f 2>&1
         fi
     done
 done
