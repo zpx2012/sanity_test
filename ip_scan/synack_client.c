4beb8d6767152b137494918a27ec8577062de478
zpx2012 <zhupengxiong@yeah.net>
2019-01-08 04:27:21 -0800

    fix

--- ip_scan/synack_client.c
+++ ip_scan/synack_client.c
@@ -18,7 +18,8 @@ int main(int argc , char *argv[])
     char message[] = "GET /sdk-tools-linux-3859397.zip HTTP/1.1\r\nHost: 169.235.31.181\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\nReferer: http://169.235.31.181/\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: zh-CN,zh;q=0.9,en;q=0.8\r\n";
 
     char* ip = argv[1];
-    int port = atoi(argv[2]);
+    int dport = atoi(argv[2]);
+    int sport = atoi(argv[3]);
 
     sock = socket(AF_INET , SOCK_STREAM , 0);
     if (sock == -1)
@@ -30,26 +31,35 @@ int main(int argc , char *argv[])
 
     server.sin_addr.s_addr = inet_addr(ip);
     server.sin_family = AF_INET;
-    server.sin_port = htons(port);
- 
-    //Connect to remote server
-    while (connect(sock, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) < 0)
-    {
-        perror("connect failed. Retry");
-       
-    }     
-    printf("Connected\n");     
+    server.sin_port = htons(dport);
 
-    while(1){
-    if((bytes = send(sock, message, strlen(message)+1, MSG_DONTWAIT)) < 0) {
-        perror("Error on sendto()");
+    client.sin_addr.s_addr = INADDR_ANY;
+    client.sin_family = AF_INET;
+    client.sin_port = htons(sport);
+
+    if (bind(sock, (struct sockaddr*) &client, sizeof(struct sockaddr_in)) < 0){
+        printf("Unable to bind\n"); 
         return -1;
-    }
-    printf("Success! Sent %d bytes.\n", bytes);
-    sleep(1);
-    }
+    } 
+    
+    while (1){
+        //Connect to remote server
+        while (connect(sock, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) < 0)
+        {
+            perror("connect failed. Retry");
+        
+        }     
+        printf("Connected\n");     
 
+        while(1){
+        if((bytes = send(sock, message, strlen(message)+1, MSG_DONTWAIT)) < 0) {
+            perror("Error on sendto()");
+            return -1;
+        }
+        printf("Success! Sent %d bytes.\n", bytes);
+        sleep(1);
+        }        
+    }
     close(sock);
-
     return 0;
 }
