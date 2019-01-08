3bafdab351dfb1109040dbff3a25b9b623532c1d
zpx2012 <zhupengxiong@yeah.net>
2019-01-08 05:08:26 -0800

    fix

--- ip_scan/synack_test.sh
+++ ip_scan/synack_test.sh
@@ -10,7 +10,7 @@ cat $1 | while IFS=' ' read ip port; do
     if [ ! -z "$rt" -a "$rt" != " " ]; then
         echo $ip $port  
         tfile=~/sanity_test_results/ptraceroute_$(hostname)_${ip}_${port}_$((n+i))_$(date -u +%m%d%H%M%S)utc_server.txt
-        echo "while true;do date -u +"'"%Y-%m-%d %H:%M:%S %Z"'">> $tfile;sudo paris-traceroute -Q -s $((n+i)) -d $portn -p tcp $ip >> $tfile;done;exec bash" > pt$i.sh
+        echo "while true;do date -u +"'"%Y-%m-%d %H:%M:%S %Z"'">> $tfile;sudo paris-traceroute -Q -s $((n+i)) -d $port -p tcp $ip >> $tfile;done;exec bash" > pt$i.sh
         screen -dmS ptr_$ip bash pt$i.sh
         cd ~/packet_trace/loss_$stime
         echo "sudo tcpdump -w tcpdump_${ip}_${port}_$(hostname)_"'%m%d%H%M%S%z'"utc.pcap -G 60 -s 96 -i $2 -n host $ip and tcp port $port;exec bash" > tmp$i.sh
