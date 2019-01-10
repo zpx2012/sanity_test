a25aece3cd076b70385b6b13c4cc17cddaafec81
zpx2012 <zhupengxiong@yeah.net>
2019-01-10 03:04:14 -0800

    fix

--- ip_scan/ip_scan.sh
+++ ip_scan/ip_scan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    sudo traceroute -A -p 80 -P tcp -f 4 -m 25 $line >> "$2" 2>&1
+    echo $line
+    sudo paris-traceroute -Q -d 80 -p tcp -f 4 -m 25 $line >> "$2" 2>&1
     echo >> "$2"
 done < "$1"
