ba7cd896877c80db247616535502ec2a335fea6a
zpx2012 <zhupengxiong@yeah.net>
2019-01-10 02:52:27 -0800

    fix

--- ip_scan/port_scan.sh
+++ ip_scan/port_scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo $line
-    sudo ~/masscan/bin/masscan $line --show closed -p0-65535 --rate $2 --append-output -oL ~/sanity_test_results/pscan_$(hostname)_$(echo $line | cut -d'/' -f1).txt
+    sudo ~/masscan/bin/masscan $line --show closed -p0-65535 --rate $2 --append-output -oL ~/sanity_test_results/pscan_$(echo $line | cut -d'/' -f1)_$(echo $line | cut -d'/' -f2)_$(hostname).txt
 done < "$1"
