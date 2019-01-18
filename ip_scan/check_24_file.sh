068eeca125c2bdab342e9db7967038473c3d9e34
zpx2012 <zhupengxiong@yeah.net>
2019-01-18 02:48:41 -0800

    fix

--- ip_scan/check_24_file.sh
+++ ip_scan/check_24_file.sh
@@ -20,12 +20,12 @@ cat $f | grep close | awk '!seen[$4]++' | head -20 | while IFS=' ' read closed t
             # rt=`cat onc | grep '100% packet loss'`
             if ! cat oncSA | grep -q '100% packet loss'; then            
                 echo SYNACK: $ip $port  
-                screen -dmS hping3_SA_$ip bash hping3.sh $ip $port $n SA u500000
+                screen -dmS hping3_SA_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n SA u500000
                 ((i++))
                 ((n++))
             elif ! cat oncS | grep -q '100% packet loss'; then
                 echo SYN: $ip $port  
-                screen -dmS hping3_S_$ip bash hping3.sh $ip $port $n S 1
+                screen -dmS hping3_S_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n S 1
                 ((i++))
                 ((n++))
             else
--- ip_scan/closed_port.sh
+++ ip_scan/closed_port.sh
@@ -2,8 +2,7 @@
 
 n=33456
 cd ~/sanity_test_results/closed_test
-rm via4134.txt
 for f in pscan_*.txt;do
-    screen -dmS 24_$n bash check_24_file.sh $f $n
+    screen -dmS 24_$n bash ~/sanity_test/ip_scan/check_24_file.sh $f $n
     ((n+=5))
 done
