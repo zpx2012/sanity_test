28a695c4129a13b8844ef84217cc468fdbced393
zpx2012 <zhupengxiong@yeah.net>
2019-01-17 22:48:10 -0800

    fix

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y python-pip libcurl4-gnutls-dev python-dev libgnutls28-dev screen traceroute dos2unix mtr paris-traceroute
+sudo apt-get install -y python-pip libcurl4-gnutls-dev python-dev libgnutls28-dev screen traceroute dos2unix mtr paris-traceroute hping3 nodejs npm
 sudo pip install pycurl numpy scipy shadowsocks psutil
+sudo npm install -g http-server
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+mkdir httpserver
+cd httpserver
+wget http://169.235.31.181/my.pcap
+screen -dmS httpserver sudo http-server -p 80
