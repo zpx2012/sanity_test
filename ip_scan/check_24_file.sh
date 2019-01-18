a1a49832a66252a557534b0cf33318f5564d8e72
zpx2012 <zhupengxiong@yeah.net>
2019-01-18 02:55:48 -0800

    fix

--- ip_scan/check_24_file.sh
+++ ip_scan/check_24_file.sh
@@ -13,19 +13,19 @@ cat $f | grep close | awk '!seen[$4]++' | head -20 | while IFS=' ' read closed t
         cat optr
         rt=`cat optr | grep \(202.97.` 
         if [ ! -z "$rt" -a "$rt" != " " ]; then
-            hping3 -SA -i u500000 -c 10 -s $n -p $port $ip 2>> oncSA 
-            hping3 -S -i u500000 -c 10 -s $n -p $port $ip 2>> oncS 
-            cat oncSA
-            cat oncS
+            hping3 -SA -i u500000 -c 10 -s $n -p $port $ip 2> oncSA_$n 
+            hping3 -S -i u500000 -c 10 -s $n -p $port $ip 2> oncS_$n 
+            cat oncSA_$n
+            cat oncS_$n
             # rt=`cat onc | grep '100% packet loss'`
-            if ! cat oncSA | grep -q '100% packet loss'; then            
+            if ! cat oncSA_$n | grep -q '100% packet loss'; then            
                 echo SYNACK: $ip $port  
-                screen -dmS hping3_SA_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n SA u500000
+                screen -dmS hping3_SA_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n SA u500000 120
                 ((i++))
                 ((n++))
-            elif ! cat oncS | grep -q '100% packet loss'; then
+            elif ! cat oncS_$n | grep -q '100% packet loss'; then
                 echo SYN: $ip $port  
-                screen -dmS hping3_S_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n S 1
+                screen -dmS hping3_S_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n S 1 60
                 ((i++))
                 ((n++))
             else
--- ip_scan/hping3.sh
+++ ip_scan/hping3.sh
@@ -2,11 +2,9 @@
 ip=$1
 dp=$2
 sp=$3
-fl=$4
-it=$5
 out=hping3_closed_${ip}_${dp}_${sp}_stdout.txt
 err=hping3_closed_${ip}_${dp}_${sp}_stderr.txt
 while true;do
 date -u +"%Y-%m-%d %H:%M:%S %Z" | tee -a $out $err
-hping3 -$fl -i $it -c 120 -s $sp -p $dp $ip >> $out 2>> $err
+hping3 -$4 -i $5 -c $6 -s $sp -p $dp $ip >> $out 2>> $err
 done
