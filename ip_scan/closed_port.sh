eec30d910cf370cdced077fb6568a614e67dcff7
zpx2012 <zhupengxiong@yeah.net>
2019-01-17 22:09:42 -0800

    fix

--- ip_scan/closed_port.sh
+++ ip_scan/closed_port.sh
@@ -8,18 +8,22 @@ i=0
 n=33456
 cd ~/sanity_test_results/closed_test
 for f in pscan_*.txt;do
-    cat $f | grep close | sort -k 4,4 -u | head -3 | while IFS=' ' read closed tcp port ip ts; do
-        sudo paris-traceroute -Q -s $((n+i)) -d $port -p tcp -f 4 -m 25 $ip > optr 2>&1
-        cat optr
-        rt=`cat optr | grep \(202.97.\)` 
-        if [ ! -z "$rt" -a "$rt" != " " ]; then
-            nc -zv $ip $port > onc 2>&1
-            rt=`cat onc | grep refused`
-            if [ ! -z "$rt" -a "$rt" != " " ]; then            
-                echo $ip $port  
-                out=hping3_closed_${ip}_${port}.txt
-                screen -dmS hping3_$ip bash -c "while true;do date -u +"'"%Y-%m-%d %H:%M:%S %Z"'" >> $out;hping3 -SA -i 1 -c 60 -s $((n+i)) -p $port $ip >> $out 2>&1;done"
-                ((i++))
+    cat $f | grep close | sort -k 4,4 -u | head -15 | while IFS=' ' read closed tcp port ip ts; do
+        if (( i < 3 ));then
+            sudo paris-traceroute -Q -s $n -d $port -p tcp -f 4 -m 25 $ip > optr 2>&1
+            cat optr
+            rt=`cat optr | grep \(202.97.` 
+            if [ ! -z "$rt" -a "$rt" != " " ]; then
+                nc -zv $ip $port > onc 2>&1
+                cat onc
+                rt=`cat onc | grep refused`
+                if [ ! -z "$rt" -a "$rt" != " " ]; then            
+                    echo $ip $port  
+                    out=hping3_closed_${ip}_${port}.txt
+                    screen -dmS hping3_$ip bash -c "while true;do date -u +"'"%Y-%m-%d %H:%M:%S %Z"'" >> $out;hping3 -SA -i 1 -c 60 -s $n -p $port $ip >> $out 2>&1;done"
+                    ((i++))
+                    ((n++))
+                fi
             fi
         fi
     done
