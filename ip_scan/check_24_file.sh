543101f3ee4ea1fb3ac63fb6071bc79c6a83ed00
zpx2012 <zhupengxiong@yeah.net>
2019-01-21 01:36:50 -0800

    fix

--- ip_scan/check_24_file.sh
+++ ip_scan/check_24_file.sh
@@ -7,32 +7,29 @@
 f=$1
 n=$2
 i=0
-arr=(211.146.20.137 211.89.228.11 211.98.235.12 211.89.235.13)
-cat "$f" | grep close | awk '!seen[$4]++' | while IFS=' ' read closed tcp port ip ts; do
-    if [[ " ${arr[*]} " =~ *" $ip "* ]];then
-        if (( i < 20 ));then
-            sudo paris-traceroute -Q -s $n -d $port -p tcp -f 4 -m 25 $ip > optr 2>&1
-            cat optr
-            rt=`cat optr | grep \(202.97.`
-            if [ ! -z "$rt" -a "$rt" != " " ]; then
-                hping3 -SA -i u500000 -c 10 -s $n -p $port $ip 2> oncSA_$n 
-                hping3 -S -i u500000 -c 10 -s $n -p $port $ip 2> oncS_$n 
-                cat oncSA_$n
-                cat oncS_$n
-                # rt=`cat onc | grep '100% packet loss'`
-                if ! cat oncSA_$n | grep -q '100% packet loss'; then            
-                    echo SYNACK: $ip $port  
-                    screen -dmS hping3_SA_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n SA 1 60
-                    ((i++))
-                    ((n++))
-                elif ! cat oncS_$n | grep -q '100% packet loss'; then
-                    echo SYN: $ip $port  
-                    screen -dmS hping3_S_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n S 1 60
-                    ((i++))
-                    ((n++))
-                else
-                    echo $ip $port >> via4134.txt
-                fi
+cat $f | grep close | awk '!seen[$4]++' | while IFS=' ' read closed tcp port ip ts; do
+    if (( i < 20 ));then
+        sudo paris-traceroute -Q -s $n -d $port -p tcp -f 4 -m 25 $ip > optr 2>&1
+        cat optr
+        rt=`cat optr | grep \(202.97.`
+        if [ ! -z "$rt" -a "$rt" != " " ]; then
+            hping3 -SA -i u500000 -c 10 -s $n -p $port $ip 2> oncSA_$n 
+            hping3 -S -i u500000 -c 10 -s $n -p $port $ip 2> oncS_$n 
+            cat oncSA_$n
+            cat oncS_$n
+            # rt=`cat onc | grep '100% packet loss'`
+            if ! cat oncSA_$n | grep -q '100% packet loss'; then            
+                echo SYNACK: $ip $port  
+                screen -dmS hping3_SA_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n SA 1 60
+                ((i++))
+                ((n++))
+            elif ! cat oncS_$n | grep -q '100% packet loss'; then
+                echo SYN: $ip $port  
+                screen -dmS hping3_S_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n S 1 60
+                ((i++))
+                ((n++))
+            else
+                echo $ip $port >> via4134.txt
             fi
         fi
     fi
