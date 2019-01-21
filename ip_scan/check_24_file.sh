8d7aba8d5ce36aa934a24bdefc2cbc5575e2da93
zpx2012 <zhupengxiong@yeah.net>
2019-01-20 17:46:50 -0800

    fix

--- ip_scan/check_24_file.sh
+++ ip_scan/check_24_file.sh
@@ -7,11 +7,11 @@
 f=$1
 n=$2
 i=0
-cat $f | grep close | awk '!seen[$4]++' | head -20 | while IFS=' ' read closed tcp port ip ts; do
-    if (( i < 5 ));then
+cat $f | grep close | awk '!seen[$4]++' | head -50 | while IFS=' ' read closed tcp port ip ts; do
+    if (( i < 15 ));then
         sudo paris-traceroute -Q -s $n -d $port -p tcp -f 4 -m 25 $ip > optr 2>&1
         cat optr
-        rt=`cat optr | grep \(202.97.` 
+        rt=`cat optr | grep \(202.97.`
         if [ ! -z "$rt" -a "$rt" != " " ]; then
             hping3 -SA -i u500000 -c 10 -s $n -p $port $ip 2> oncSA_$n 
             hping3 -S -i u500000 -c 10 -s $n -p $port $ip 2> oncS_$n 
@@ -20,7 +20,7 @@ cat $f | grep close | awk '!seen[$4]++' | head -20 | while IFS=' ' read closed t
             # rt=`cat onc | grep '100% packet loss'`
             if ! cat oncSA_$n | grep -q '100% packet loss'; then            
                 echo SYNACK: $ip $port  
-                screen -dmS hping3_SA_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n SA u500000 120
+                screen -dmS hping3_SA_$ip bash ~/sanity_test/ip_scan/hping3.sh $ip $port $n SA 1 60
                 ((i++))
                 ((n++))
             elif ! cat oncS_$n | grep -q '100% packet loss'; then
--- ip_scan/closed_port.sh
+++ ip_scan/closed_port.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 n=33456
-cd ~/sanity_test_results/closed_test
+cd $1
 for f in pscan_*.txt;do
     screen -dmS 24_$n bash ~/sanity_test/ip_scan/check_24_file.sh $f $n
     ((n+=5))
