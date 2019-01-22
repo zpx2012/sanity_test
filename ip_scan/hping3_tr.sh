b0da8770959e94d0d9cac9c2cc4a7cb5ba294183
zpx2012 <zhupengxiong@yeah.net>
2019-01-22 02:13:55 -0800

    fix

--- ip_scan/hping3_tr.sh
+++ ip_scan/hping3_tr.sh
@@ -2,8 +2,8 @@
 ip=$1
 dp=$2
 sp=$3
-out=~/sanity_test_results/hping3_tr_${ip}_${dp}_${sp}_${4}_stdout_$(hostname)_$(date -u +"%m%d%H%M").txt
-err=~/sanity_test_results/hping3_tr_${ip}_${dp}_${sp}_${4}_stderr_$(hostname)_$(date -u +"%m%d%H%M").txt
+out=~/sanity_test_results/hping3_tr_${ip}_${dp}_${sp}_${4}_$7_stdout_$(hostname)_$(date -u +"%m%d%H%M").txt
+err=~/sanity_test_results/hping3_tr_${ip}_${dp}_${sp}_${4}_$7_stderr_$(hostname)_$(date -u +"%m%d%H%M").txt
 while true;do
 date -u +"%Y-%m-%d %H:%M:%S %Z" | tee -a $out $err
 hping3 -$4 -i $5 -c $6 --traceroute --tr-keep-ttl -t $7 -s $sp -k -p $dp $ip >> $out 2>> $err
