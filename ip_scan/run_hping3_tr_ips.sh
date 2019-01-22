4afd462c9e681c556a82ca03e96b2a383aedd27c
zpx2012 <zhupengxiong@yeah.net>
2019-01-22 02:07:09 -0800

    fix

--- ip_scan/run_hping3_tr_ips.sh
+++ ip_scan/run_hping3_tr_ips.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 n=7
 cat $1 | while IFS=' ' read ip port; do
-    if (( n < 10 ));then
-        screen -dmS hping3_tr_$ip bash ~/sanity_test/ip_scan/hping3_tr.sh $ip $port $n SA u500000 120 $n
+    while (( n < 10 ));then
+        screen -dmS hping3_tr_$ip_$n bash ~/sanity_test/ip_scan/hping3_tr.sh $ip $port $n SA u500000 120 $n
         ((n++))
-    fi
+    done
+    n=7
 done
