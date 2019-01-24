7ae254c7990ab7720794c566597af74b043e0ff0
zpx2012 <zhupengxiong@yeah.net>
2019-01-24 03:13:22 -0800

    fix

---
+++ ip_scan/tr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ip=$1
+dp=$2
+sp=$3
+tfile=~/sanity_test_results/ptraceroute_$(hostname)_${ip}_${dp}_${sp}_$(date -u +%m%d%H%M%S).txt
+while true;do 
+    date -u +"%Y-%m-%d %H:%M:%S %Z" >> $tfile
+    sudo traceroute -A --sport=$sp -p $dp -T -f 4 -m 25 $ip >> $tfile
+done
+exec bash
