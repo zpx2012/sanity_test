84b65dc1a000f40e766b5ae839bbf6213a05dcb8
zpx2012 <zhupengxiong@yeah.net>
2019-01-29 03:42:59 -0800

    fix

--- ip_scan/path_col_l2_0129.sh
+++ ip_scan/path_col_l2_0129.sh
@@ -1,24 +1,28 @@
 f=$1
 n=$2
-trfile=$(echo ${f/pscan/tr}| sed -e 's/.txt//g')_$(hostname)_$(date -u +"%m%d%H%M").txt
+i=0
+trfile=~/sanity_test_results/$(echo ${f/pscan/tr}| sed -e 's/.txt//g')_$(hostname)_$(date -u +"%m%d%H%M").txt
 cat $f | while IFS=' ' read closed tcp port ip ts; do
-    sudo traceroute -A --sport=$n -p $port -T -f 4 -m 25 $ip > otr_$ip
-    cat otr_$ip
-    cat otr_$ip >> $trfile
-    rt=`cat otr | grep -e '202\.97\.\|AS4134'`
-    if [ ! -z "$rt" -a "$rt" != " " ]; then
-        echo $ip $port >> via4134.txt
-        hping3 -SA -i u500000 -c 10 -s $n -p $port $ip 2> oncSA_$n;cat oncSA_$n
-        hping3 -S -i u500000 -c 10 -s $n -p $port $ip 2> oncS_$n;cat oncS_$n
-        # rt=`cat onc | grep '100% packet loss'`
-        if ! cat oncSA_$n | grep -q '100% packet loss'; then            
-            echo SYNACK: $ip $port  
-            screen -dmS hping3_SA_$ip bash ~/sanity_test/ip_scan/hping3_ptr.sh $ip $port $n SA 1 60
-            ((n++))
-        elif ! cat oncS_$n | grep -q '100% packet loss'; then
-            echo SYN: $ip $port  
-            screen -dmS hping3_S_$ip bash ~/sanity_test/ip_scan/hping3_ptr.sh $ip $port $n S 1 60
-            ((n++))
+    if ((i < 5));then
+        sudo traceroute -A --sport=$n -p $port -T -f 4 -m 25 $ip > otr_$ip
+        cat otr_$ip
+        cat otr_$ip >> $trfile
+        rt=`cat otr_$ip | grep -e '202\.97\.\|AS4134'`
+        if [ ! -z "$rt" -a "$rt" != " " ]; then
+            ((i++))
+            echo $ip $port >> via4134.txt
+            hping3 -SA -i u500000 -c 10 -s $n -p $port $ip 2> oncSA_$n;cat oncSA_$n
+            hping3 -S -i u500000 -c 10 -s $n -p $port $ip 2> oncS_$n;cat oncS_$n
+            # rt=`cat onc | grep '100% packet loss'`
+            if ! cat oncSA_$n | grep -q '100% packet loss'; then            
+                echo SYNACK: $ip $port  
+                screen -dmS hping3_SA_$ip bash ~/sanity_test/ip_scan/hping3_ptr.sh $ip $port $n SA 1 60
+                ((n++))
+            elif ! cat oncS_$n | grep -q '100% packet loss'; then
+                echo SYN: $ip $port  
+                screen -dmS hping3_S_$ip bash ~/sanity_test/ip_scan/hping3_ptr.sh $ip $port $n S 1 60
+                ((n++))
+            fi
         fi
     fi
 done
