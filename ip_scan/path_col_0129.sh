7b3102041febb11b90b0ff812119a35d17e6da0c
zpx2012 <zhupengxiong@yeah.net>
2019-01-29 03:53:36 -0800

    fix

--- ip_scan/path_col_0129.sh
+++ ip_scan/path_col_0129.sh
@@ -3,12 +3,12 @@ i=0
 n=`shuf -i 1024-65535 -n 1`
 cd ~/sanity_test/ip_scan/data/sp20
 for f in $(for n in $(seq 1 60|shuf);do sed $n'!d' file_list;done);do 
-    if (( i < 100 )); then
+    if (( i < 150 )); then
     screen -dmS 24_$n bash ~/sanity_test/ip_scan/path_col_l2_0129.sh $f $n
     ((n+=20))
     ((i++))
     else
-        sleep 180
-        i=0
+        i=(`screen -ls | grep 'in /var/run/screen/S-root'`)
+        ((i-=1))
     fi
 done
