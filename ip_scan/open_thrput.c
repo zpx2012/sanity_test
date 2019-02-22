22ab4a815cf5bc5574b5bbd244e92df84ddeffd0
zpx2012 <zhupengxiong@yeah.net>
2019-02-22 04:57:48 -0800

    fix

--- ip_scan/open_thrput.c
+++ ip_scan/open_thrput.c
@@ -233,7 +233,7 @@ int main(int argc , char *argv[])
     gettimeofday(&ses_last_tv, NULL);
     gettimeofday(&pkt_last_tv,NULL);
     fpkt_intvl_ts.tv_sec = 0;
-    fpkt_intvl_ts.tv_usec = 188;
+    fpkt_intvl_ts.tv_nsec = 188541ix;
     while(1){        
             gettimeofday(&ses_this_tv, NULL);
             timersub(&ses_this_tv,&ses_last_tv,&ses_intvl_tv);
