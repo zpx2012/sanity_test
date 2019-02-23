0884388a4bb161dfc1bcc099be24ea7152554389
zpx2012 <zhupengxiong@yeah.net>
2019-02-23 02:34:19 -0800

    fix

--- ip_scan/0222_mtr_poll.sh
+++ ip_scan/0222_mtr_poll.sh
@@ -1,8 +1,8 @@
 f=$1
 mtr=$2
 while true;do
-    cat $1 | while IFS=' ' read ip port; do
-        sudo $mtr -zwnr4T -P $port -c 100 $ip 2>&1 | tee -a ~/sanity_test_results/mtr_data_$(hostname)_${ip}_${port}_tcp_1_100_$startime.txt
+    cat $f | while IFS=' ' read ip port; do
+        sudo $mtr -zwnr4T -P $port -c 100 $ip 2>&1 | tee -a ~/sanity_test_results/mtr_data_$(hostname)_${ip}_${port}_tcp_1_100_$(date -u +"%m%d%H%M").txt
     done
 done
-    
+exec bash    
