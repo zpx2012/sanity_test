7dc6575fca887c871483048667623db60f9acbae
zpx2012 <zhupengxiong@yeah.net>
2019-02-24 02:51:46 -0800

    0224

--- ip_scan/0222_mtr.sh
+++ ip_scan/0222_mtr.sh
@@ -4,8 +4,8 @@ gcc open_thrput.c -o open_thrput.o
 i=0
 n=`shuf -i 1024-65535 -n 1`
 mtr=~/mtr-modified/mtr
-tf=test_$(date -u +"%m%d%H%M")
-cat ~/sanity_test/ip_scan/data/0222_mtr/via4134.txt | while IFS=' ' read ip port; do
+comm -13 ~/sanity_test/ip_scan/test_022311* ~/sanity_test/ip_scan/data/0222_mtr/via4134.txt | while IFS=' ' read ip port; do
+    tf=~/sanity_test/ip_scan/data/0222_mtr/test_$(date -u +"%m%d%H%M")
     if ((i < 3));then
         sudo $mtr -zwnr4T -P $port $ip > otr_$ip 2>&1
         cat otr_$ip
@@ -15,17 +15,23 @@ cat ~/sanity_test/ip_scan/data/0222_mtr/via4134.txt | while IFS=' ' read ip port
             echo $ip $port >> $tf
             screen -dmS td_$ip bash ~/sanity_test/ip_scan/tcpdump_whole.sh $ip $port $ip
             screen -dmS opt_$ip bash -c "while true;do ~/sanity_test/ip_scan/open_thrput.o $ip $port $n;done;exec bash"
-            ((i++))
-            ((n++))
+            let i=i+1
+            let n=n+1
+        else 
+            echo $ip $port >> ~/sanity_test/ip_scan/data/0222_mtr/no4134_$(date -u +"%m%d%H%M")
         fi
     else
-        break
+        screen -dmS mtr bash ~/sanity_test/ip_scan/0222_mtr_poll.sh $tf $mtr
+        sleep 86400
+        bash ~/sanity_test/ks.sh mtr
+        bash ~/sanity_test/ks.sh td
+        bash ~/sanity_test/ks.sh opt
+        let i=0
     fi
 done
-if [ -s $tf ];then
+if ((i > 0));then
     screen -dmS mtr bash ~/sanity_test/ip_scan/0222_mtr_poll.sh $tf $mtr
 fi
-exec bash
 # 
 # for f in $(for n in $(seq 1 1000|shuf);do sed $n'!d' file_list;done);do 
 #     if (( i < 20 )); then
