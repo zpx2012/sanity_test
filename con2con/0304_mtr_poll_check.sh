d03aecfaeab20a04279e14aef6737003afe1bc9d
zpx2012 <zhupengxiong@yeah.net>
2019-03-06 01:52:26 -0800

    fix

--- con2con/0304_mtr_poll_check.sh
+++ con2con/0304_mtr_poll_check.sh
@@ -4,11 +4,14 @@ mtr=$2
 start=$(date -u +"%m%d%H%M")
 i=30
 while true;do
-    if (( i == 30 ));then #check
+    if (( i == 15 ));then #check
         cp /dev/null $fm
         cat $f | while IFS=' ' read ip hn dp sp; do
             sudo $mtr -zwnr4T -P $dp -L $sp -i 0.1 -c 50 $ip 2>&1 | tee tmp
             max=`cat tmp | tail -n1 | sed 's@^[^0-9]*\([0-9]\+\).*@\1@'`
+            if (( max < 10 ));then
+                let max=19
+            fi
             echo $ip $hn $dp $sp $((max+1)) >> $fm
             cat tmp >> ~/sanity_test_results/mtr_data_$(hostname)_${hn}_${sp}_tcp_0.1_50_check_${start}.txt
         done
