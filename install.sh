c2aa5e784c5ae634857f6de17c852e78f3f55012
zpx2012 <zhupengxiong@yeah.net>
2019-03-10 17:00:05 -0700

    fix install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ sudo npm install -g http-server
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 mkdir ~/httpserver
 cd ~/httpserver
-wget http://104.248.180.114/my.pcap
+mv ~/sanity_test/my.pcap ~/httpserver 
 screen -dmS httpserver sudo http-server -p 80
 mkdir ~/sanity_test_results/
