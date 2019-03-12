699b84666b46dbe003047069977d75c442699fb7
zpx2012 <zhupengxiong@yeah.net>
2019-03-12 01:29:23 -0700

    fix

--- ip_scan/hping3.sh
+++ ip_scan/hping3.sh
@@ -6,5 +6,5 @@ out=~/sanity_test/rs/hping3_closed_${ip}_${dp}_${sp}_${4}_stdout_$(hostname)_$(d
 err=~/sanity_test/rs/hping3_closed_${ip}_${dp}_${sp}_${4}_stderr_$(hostname)_$(date -u +"%m%d%H%M").txt
 while true;do
 date -u +"%Y-%m-%d %H:%M:%S %Z" | tee -a $out $err
-hping3 -$4 -i $5 -c $6 -s $sp -k -p $dp $ip >> $out 2>> $err
+sudo hping3 -$4 -i $5 -c $6 -s $sp -k -p $dp $ip >> $out 2>> $err
 done
