2d5d421ed3782155ebe99ae8be9cb77698f6589f
zpx2012 <zhupengxiong@yeah.net>
2019-03-19 04:50:40 -0700

    mtr don't need a extra source port number

--- o2c/0319_cn.sh
+++ o2c/0319_cn.sh
@@ -6,7 +6,7 @@ for day_i in 0 1 2 3; do
     dfile=node${node_i}_day${day_i}.csv
     cat $dfile | while IFS=' ' read ip hn dp sp; do
         screen -dmS td_$hn bash ~/sanity_test/ip_scan/tcpdump_whole.sh $ip $dp $hn c
-        screen -dmS curl_$hn python ~/sanity_test/curl_downloader.py "http://$ip/my.pcap" $ip $hn 0 1000k 0 $((sp+1))
+        screen -dmS curl_$hn python ~/sanity_test/curl_downloader.py "http://$ip/my.pcap" $ip $hn 0 1000k 0 $sp
     done
     screen -dmS mtr bash ~/sanity_test/con2con/0304_mtr_poll_check.sh $dfile $mtr
     sleep 86400
