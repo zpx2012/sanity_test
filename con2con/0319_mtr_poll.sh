badcdf211b20411f251e621e943ade9deed39609
zpx2012 <zhupengxiong@yeah.net>
2019-03-19 05:42:28 -0700

    fix

--- con2con/0319_mtr_poll.sh
+++ con2con/0319_mtr_poll.sh
@@ -3,6 +3,7 @@ mtr=$2
 start=$(date -u +"%m%d%H%M")
 while true;do
     cat $f | while IFS=' ' read ip hn dp sp; do
+        echo $ip $hn $dp $sp
         sudo $mtr -zwnr4T -P $dp -c 100 $ip 2>&1 | tee -a ~/sanity_test/rs/mtr_data_$(hostname)_${hn}_${sp}_tcp_1_100_${start}.txt
     done
 done
