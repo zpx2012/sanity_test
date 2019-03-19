59f7a22836505410f2b43304285ba31dbd9da28d
zpx2012 <zhupengxiong@yeah.net>
2019-03-19 04:03:18 -0700

    fix

--- con2con/0304_mtr_poll_check.sh
+++ con2con/0304_mtr_poll_check.sh
@@ -6,7 +6,7 @@ i=15
 while true;do
     if (( i == 15 ));then #check
         cp /dev/null $fm
-        cat $f | while IFS=' ' read ip hn dp sp; do
+        cat $f | while IFS=',' read ip hn dp sp; do
             sudo $mtr -zwnr4T -P $dp -i 0.1 -c 20 $ip 2>&1 | tee tmp
             max=`cat tmp | tail -n1 | sed 's@^[^0-9]*\([0-9]\+\).*@\1@'`
             if (( max < 10 ));then
