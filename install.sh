309a7b637cc97b46b023b226611df2e900f1c08f
zpx2012 <zhupengxiong@yeah.net>
2019-03-19 01:53:01 -0700

    add file for 0319 o2c test

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ sudo apt-get update
 sudo apt-get install -y gcc python-pip libcurl4-gnutls-dev python-dev libgnutls28-dev screen traceroute dos2unix paris-traceroute hping3 nodejs npm automake
 sudo pip install pycurl numpy scipy shadowsocks psutil
 mkdir ~/sanity_test/rs/
+git submodule update --init --recursive
