8739974deb012fbd827dc05d9ba35f013bd617a2
zpx2012 <zhupengxiong@yeah.net>
2019-03-18 22:48:43 -0700

    * install.sh + install_httpserver.sh + httpserver/my.pcap

--- install.sh
+++ install.sh
@@ -2,9 +2,4 @@
 sudo apt-get update
 sudo apt-get install -y gcc python-pip libcurl4-gnutls-dev python-dev libgnutls28-dev screen traceroute dos2unix paris-traceroute hping3 nodejs npm automake
 sudo pip install pycurl numpy scipy shadowsocks psutil
-sudo npm install -g http-server
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-mkdir ~/httpserver
-mv ~/sanity_test/my.pcap ~/httpserver 
-#screen -dmS httpserver sudo http-server -p 80
 mkdir ~/sanity_test/rs/
---
+++ install_httpserver.sh
@@ -0,0 +1,4 @@
+sudo npm install -g http-server
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+cd httpserver
+screen -dmS httpserver sudo http-server -p 80
