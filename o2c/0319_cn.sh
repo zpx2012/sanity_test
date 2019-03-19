d4ce212f46bcaa824c58c24a072f3907f612db2d
zpx2012 <zhupengxiong@yeah.net>
2019-03-19 03:50:52 -0700

    fix

--- o2c/0319_cn.sh
+++ o2c/0319_cn.sh
@@ -3,7 +3,7 @@ cd ~/sanity_test/o2c
 node_i=$1
 mtr=~/sanity_test/mtr-insertion/mtr
 for day_i in 0 1 2 3; do
-    dfile=day${day_i}_node${node_i}.csv
+    dfile=node${node_i}_day${day_i}.csv
     cat $dfile | while IFS=',' read ip hn dp sp; do
         screen -dmS td_$hn bash ~/sanity_test/ip_scan/tcpdump_whole.sh $ip $dp $hn c
         screen -dmS curl_$hn python ~/sanity_test/curl_downloader.py "http://$ip/my.pcap" $ip $hn 0 1000k 0 $((sp+1))
