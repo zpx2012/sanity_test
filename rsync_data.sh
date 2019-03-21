412be76dc56818d4656afc7e38846d71a4702cb5
zpx2012 <zhupengxiong@yeah.net>
2019-03-20 21:49:35 -0700

    add rsyn_data

--- rsync_data.sh
+++ rsync_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+outd=st_data/$(hostname)
+mv ~/sanity_test/ip_scan/data/terran ~/.ssh/
+ssh pzhu011@terran mkdir -p $outd
 while true; do
-
+    rsync -avzS --progress --remove-source-files ~/sanity_test/rs/* pzhu011@terran.cs.ucr.edu:$outd/
+    sleep 3600
 done
