d6ddf4153515b134716ea4a73d9dab96f04aeea3
zpx2012 <zhupengxiong@yeah.net>
2019-03-21 02:04:08 -0700

    fix

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod 400 ~/.ssh/id_rsa
 outd=/data/pzhu/st-data/$(hostname)
-ssh pzhu011@terran mkdir -p $outd
+ssh pzhu011@terran.cs.ucr.edu mkdir -p $outd
 while true; do
     rsync -avzS --progress --remove-source-files ~/sanity_test/rs/* pzhu011@terran.cs.ucr.edu:$outd/
     sleep 3600
