99d997d21454d28397c6eaa18093662e08804272
zpx2012 <zhupengxiong@yeah.net>
2019-03-25 02:16:04 -0700

    fix port num

--- o2c/0319_other.sh
+++ o2c/0319_other.sh
@@ -3,7 +3,7 @@ cd ~/sanity_test/o2c
 mtr=~/sanity_test/mtr-insertion/mtr
 for day_i in 0 1 2 3; do
     dfile=$(hostname)_$(date -u +"%m%d").csv
-    screen -dmS td bash ~/sanity_test/ip_scan/tcpdump_server.sh $sp
+    screen -dmS td bash ~/sanity_test/ip_scan/tcpdump_server.sh 80
     screen -dmS mtr bash ~/sanity_test/con2con/0304_mtr_poll_check.sh $dfile $mtr
     sleep 86400
     bash ~/sanity_test/ks.sh mtr
