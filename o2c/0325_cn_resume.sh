75ea2538c6baef203158ada7ab1883b0b9dc0aca
zpx2012 <zhupengxiong@yeah.net>
2019-03-25 22:57:44 -0700

    fix

--- o2c/0325_cn_resume.sh
+++ o2c/0325_cn_resume.sh
@@ -5,16 +5,16 @@ if [ -z "$1" || -z "$2" || -z "$3"]
     exit 1
 fi
 node_i=$1
-day_s=$2
+today=$2
 st_in=$3
 st_out=`date --date="$st_in" +"%m%d%H%M"`
 st_sec=`date --date="$st_in" +"%s"`
 mtr=~/sanity_test/mtr-insertion/mtr
 
 cd ~/sanity_test/o2c
-dfile=node${node_i}_day${day_i}.csv
+dfile=node${node_i}_day${today}.csv
 cat $dfile | while IFS=' ' read ip hn dp sp; do
-    screen -dmS td_$hn bash ~/sanity_test/ip_scan/tcpdump_whole_resume.sh $ip $dp $hn c $st_out
+    screen -dmS td_$hn bash ~/sanity_test/ip_scan/tcpdump_whole.sh $ip $dp $hn c
     screen -dmS curl_$hn python ~/sanity_test/curl_downloader_resume.py "http://$ip/my.pcap" $ip $hn 0 1000k 0 $sp $st_out
 done
 screen -dmS mtr bash ~/sanity_test/con2con/0304_mtr_poll_check_resume.sh $dfile $mtr $st_out
@@ -24,7 +24,7 @@ bash ~/sanity_test/ks.sh mtr
 bash ~/sanity_test/ks.sh td
 bash ~/sanity_test/ks.sh curl
 
-for ((day_i=day_s; day_i<4; day_i++));; do
+for ((day_i=today+1; day_i<4; day_i++));; do
     dfile=node${node_i}_day${day_i}.csv
     cat $dfile | while IFS=' ' read ip hn dp sp; do
         screen -dmS td_$hn bash ~/sanity_test/ip_scan/tcpdump_whole.sh $ip $dp $hn c
