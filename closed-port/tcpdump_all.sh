d2507c3bc4be26bd7306457c5ed2c0ba99c4fa5c
zpx2012 <zhupengxiong@yeah.net>
2019-04-08 10:42:10 -0700

    fix

--- closed-port/tcpdump_all.sh
+++ closed-port/tcpdump_all.sh
@@ -1,4 +1,4 @@
 inf=`ip route get 8.8.8.8 | head -n1 | awk -- '{print $5x}'`
 outdir=~/sanity_test/rs/tcpdump_$(date -u +%m%d)
 mkdir $outdir
-sudo tcpdump -w $outdir/tcpdump_$(hostname)_$(date -u +%m%d%H%M)_server.pcap -vv -W 2000 -C 1024 -s 96 -i $inf
+sudo tcpdump -w $outdir/tcpdump_$(hostname)_$(date -u +%m%d%H%M)_server.pcap -vv -W 2000 -C 1024 -s 96 -i $inf port not 22
