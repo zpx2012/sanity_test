6c2c4c473e83c8cec68a6bf6afe5a58c7d0af901
zpx2012 <zhupengxiong@yeah.net>
2019-04-09 01:46:00 -0700

    fix

--- closed-port/0406_p2.sh
+++ closed-port/0406_p2.sh
@@ -3,4 +3,5 @@ cd ~/sanity_test/closed-port
 mtr=~/sanity_test/mtr-modified/mtr
 for day_i in 0 1 2 3 4; do
     screen -dmS mtr4 bash 0406_mtr_poll.sh $(hostname)_day${day_i}_p2.csv $mtr
+    sleep 86400
 done
