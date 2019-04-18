4b2f2235dc105f42dce09b784ef2a39bf5e946db
zpx2012 <zhupengxiong@yeah.net>
2019-04-17 23:36:27 -0700

    modified:   prot/0417_cn.sh

--- prot/0417_cn.sh
+++ prot/0417_cn.sh
@@ -5,7 +5,7 @@ for day in 0 1;do
     cat data/$(hostname)_day${day}.csv | while IFS=' ' read ip hn http_sp https_sp ss_lp ss_sp iperf_dp iperf_sp; do
         screen -dmS td_$hn bash tcpdump_iponly.sh $ip $hn
         screen -dmS ss_$hn sslocal -c ../shadowsocks/${hn}_client.json
-        screen -dmS poller_$hn poller.sh $ip $hn $http_sp $https_sp $ss_lp $ss_sp $iperf_dp $iperf_sp
+        screen -dmS poller_$hn bash poller.sh $ip $hn $http_sp $https_sp $ss_lp $ss_sp $iperf_dp $iperf_sp
         screen -dmS mtr_$hn bash ~/sanity_test/con2con/0304_mtr_poll.sh data/mtr_$(hostname)_${hn}.csv $mtr
     done
     sleep 86400
