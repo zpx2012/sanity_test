7fb4d3ce01dd7d6bbce08bb18c4d4c90edc15623
zpx2012 <zhupengxiong@yeah.net>
2019-04-18 03:06:13 -0700

    changed mtr poll to all simo

--- prot/0417_ot.sh
+++ prot/0417_ot.sh
@@ -3,8 +3,11 @@ cd ~/sanity_test/prot
 mtr=~/sanity_test/mtr-insertion/mtr
 for day in 0 1;do
     screen -dmS td bash tcpdump_portonly.sh
-    cat data/$(hostname)_day${day}.csv | while IFS=' ' read ip hn ; do
-        screen -dmS mtr bash ~/sanity_test/con2con/0319_mtr_poll.sh data/mtr_$(hostname)_${hn}.csv $mtr
+    cat data/$(hostname)_day${day}.csv | while IFS=' ' read ip hn http_sp https_sp ss_lp ss_sp iperf_dp iperf_sp; do
+        screen -dmS mtr_${hn}_http bash mtr.sh $ip $hn $http_sp 80 60 $mtr
+        screen -dmS mtr_${hn}_ssl bash mtr.sh $ip $hn $https_sp 443 60 $mtr
+        screen -dmS mtr_${hn}_ss bash mtr.sh $ip $hn $ss_lp 8838 60 $mtr
+        screen -dmS mtr_${hn}_iperf bash mtr.sh $ip $hn $iperf_sp $iperf_dp 60 $mtr 
     done
     sleep 86400
     bash ~/sanity_test/ks.sh mtr
