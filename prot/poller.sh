5fd393dd35d9c52248442b2d7109f8a93f1c0be6
zpx2012 <zhupengxiong@yeah.net>
2019-04-18 00:03:11 -0700

    modified:   prot/poller.sh

--- prot/poller.sh
+++ prot/poller.sh
@@ -10,15 +10,15 @@ iperf_sp=$8
 url="${ip}/my.pcap"
 start=$(date -u +"%m%d%H%Mutc")
 while true;do
-echo http
-curl -LJv4k -o /dev/null --limit-rate 750k -m 10 --speed-time 120 --local-port $http_sp http://$url 2>&1 | tee -a ~/sanity_test/rs/curl_$(hostname)_${hn}_http_${start}_${http_sp}.txt 
+echo http $http_sp
+curl -LJv4k -o /dev/null --limit-rate 750k -m 20 --speed-time 120 --local-port $http_sp http://$url 2>&1 | tee -a ~/sanity_test/rs/curl_$(hostname)_${hn}_http_${start}_${http_sp}.txt 
 echo
-echo https
-curl -LJv4k -o /dev/null --limit-rate 750k -m 10 --speed-time 120 --local-port $https_sp https://$url 2>&1 | tee -a ~/sanity_test/rs/curl_$(hostname)_${hn}_https_${start}_${https_sp}.txt
+echo https $https_sp
+curl -LJv4k -o /dev/null --limit-rate 750k -m 20 --speed-time 120 --local-port $https_sp https://$url 2>&1 | tee -a ~/sanity_test/rs/curl_$(hostname)_${hn}_https_${start}_${https_sp}.txt
 echo
-echo ss
-curl -LJv4k -o /dev/null --limit-rate 750k -m 10 --speed-time 120 --local-port $ss_sp --socks localhost:$ss_lp 2>&1 http://$url | tee -a ~/sanity_test/rs/curl_$(hostname)_${hn}_ss_${start}_${ss_sp}.txt
+echo ss $ss_sp
+curl -LJv4k -o /dev/null --limit-rate 750k -m 20 --speed-time 120 --local-port $ss_sp --socks localhost:$ss_lp 2>&1 http://$url | tee -a ~/sanity_test/rs/curl_$(hostname)_${hn}_ss_${start}_${ss_sp}.txt
 echo
-echo iperf3
-sudo iperf3 -c $ip -p $iperf_dp --cport iperf_sp -b 750K -f K -t 10 -4VR --logfile ~/sanity_test/rs/iperf3_$(hostname)_${hn}_${iperf_dp}_${iperf_sp}_${start}.txt
+echo iperf3 $iperf_sp
+sudo iperf3 -c $ip -p $iperf_dp --cport $iperf_sp -b 750K -f K -t 20 -4VR --logfile ~/sanity_test/rs/iperf3_$(hostname)_${hn}_${iperf_dp}_${iperf_sp}_${start}.txt
 done
