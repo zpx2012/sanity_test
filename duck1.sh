c88295f0aadb6349172e40428818c412de52dc6b
zpx2012 <zhupengxiong@yeah.net>
2019-04-18 00:49:16 -0700

    modified:   duck1.sh

--- duck1.sh
+++ duck1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 while true;do
     echo url="https://www.duckdns.org/update?domains="$(hostname| tr '[:upper:]' '[:lower:]')"-kayodi&token=d349b351-647b-4430-807e-08ad5adacb74&ip=" | curl -k -K - >> ~/sanity_test/duck.log
-    date -u +"%Y-%m-%d %H:%M:%S UTC" >> ~/sanity_test/duck.log
-    nslookup $(hostname| tr '[:upper:]' '[:lower:]')-kayodi.duckdns.org >> ~/sanity_test/duck.log
+    echo
+    date -u +"%Y-%m-%d %H:%M:%S UTC" | tee -a ~/sanity_test/duck.log
+    nslookup $(hostname| tr '[:upper:]' '[:lower:]')-kayodi.duckdns.org | tee -a ~/sanity_test/duck.log
     sleep 60
 done
