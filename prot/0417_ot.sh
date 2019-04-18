8b8121693dd55f747f67eb3c3d12dae05a8e67e2
zpx2012 <zhupengxiong@yeah.net>
2019-04-18 00:21:16 -0700

    prot/0417_ot.sh: fix mtr script filename

--- prot/0417_ot.sh
+++ prot/0417_ot.sh
@@ -4,7 +4,7 @@ mtr=~/sanity_test/mtr-modified/mtr
 for day in 0 1;do
     screen -dmS td bash tcpdump_portonly.sh
     cat data/$(hostname)_day${day}.csv | while IFS=' ' read ip hn ; do
-        screen -dmS mtr bash ~/sanity_test/con2con/0304_mtr_poll.sh data/mtr_$(hostname)_${hn}.csv $mtr
+        screen -dmS mtr bash ~/sanity_test/con2con/0319_mtr_poll.sh data/mtr_$(hostname)_${hn}.csv $mtr
     done
     sleep 86400
     bash ~/sanity_test/ks.sh mtr
