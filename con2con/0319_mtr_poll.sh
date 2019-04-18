2bd1bacd854cb3df4ce3894154ce8817dd177da8
zpx2012 <zhupengxiong@yeah.net>
2019-04-17 23:33:18 -0700

    modified:   con2con/0319_mtr_poll.sh 	modified:   prot/0417_cn.sh

--- con2con/0319_mtr_poll.sh
+++ con2con/0319_mtr_poll.sh
@@ -4,7 +4,7 @@ start=$(date -u +"%m%d%H%M")
 while true;do
     cat $f | while IFS=' ' read ip hn dp sp; do
         echo $ip $hn $dp $sp
-        sudo $mtr -zwnr4T -P $dp -L $((sp+1)) -c 100 $ip 2>&1 | tee -a ~/sanity_test/rs/mtr_data_$(hostname)_${hn}_${sp}_tcp_1_100_${start}.txt
+        sudo $mtr -zwnr4T -P $dp -L $((sp+1)) -c 100 $ip 2>&1 | tee -a ~/sanity_test/rs/mtrins_$(hostname)_${hn}_${sp}_tcp_1_100_${start}.txt
     done
 done
 exec bash    
