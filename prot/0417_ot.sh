94e940a4489d7fe27c2c9d7b11b6c56fcd3a4926
zpx2012 <zhupengxiong@yeah.net>
2019-04-19 15:09:15 -0700

    mtr output filename modified:   prot/0417_cn.sh modified:   prot/0417_ot.sh modified:   prot/mtr.sh

--- prot/0417_ot.sh
+++ prot/0417_ot.sh
@@ -4,10 +4,10 @@ mtr=~/sanity_test/mtr-insertion/mtr
 for day in 0 1;do
     screen -dmS td bash tcpdump_portonly.sh
     cat data/$(hostname)_day${day}.csv | while IFS=' ' read ip hn http_sp https_sp ss_lp ss_sp iperf_dp iperf_sp; do
-        screen -dmS mtr_${hn}_http bash mtr.sh $ip $hn $http_sp 80 60 $mtr
-        screen -dmS mtr_${hn}_ssl bash mtr.sh $ip $hn $https_sp 443 60 $mtr
-        screen -dmS mtr_${hn}_ss bash mtr.sh $ip $hn $ss_lp 8388 60 $mtr
-        screen -dmS mtr_${hn}_iperf bash mtr.sh $ip $hn $iperf_sp $iperf_dp 60 $mtr 
+        screen -dmS mtr_${hn}_http bash mtr.sh $ip $hn $http_sp 80 60 $mtr http
+        screen -dmS mtr_${hn}_ssl bash mtr.sh $ip $hn $https_sp 443 60 $mtr https
+        screen -dmS mtr_${hn}_ss bash mtr.sh $ip $hn $ss_lp 8388 60 $mtr ss
+        screen -dmS mtr_${hn}_iperf bash mtr.sh $ip $hn $iperf_sp $iperf_dp 60 $mtr iperf3
     done
     sleep 86400
     bash ~/sanity_test/ks.sh mtr
--- prot/mtr.sh
+++ prot/mtr.sh
@@ -5,8 +5,9 @@ dp=$3
 sp=$4
 cnt=$5
 mtr=$6
+prot=$7
 startime=$(date -u +"%m%d%H%M")utc
 while true
 do 
-sudo $mtr -zwnr4T -P $dp -L $sp -c $cnt $ip | tee -a ~/sanity_test/rs/mtrins_$(hostname)_${hn}_${dp}_${sp}_tcp_1_${cnt}_$startime.txt
+sudo $mtr -zwnr4T -P $dp -L $sp -c $cnt $ip | tee -a ~/sanity_test/rs/mtrins_$(hostname)_${hn}_${prot}_${dp}_${sp}_tcp_1_${cnt}_$startime.txt
 done
