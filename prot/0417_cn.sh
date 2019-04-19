755e8ff75fb4559c2faad6f947a391e88f1e0a1e
zpx2012 <zhupengxiong@yeah.net>
2019-04-19 04:32:28 -0700

    ss port wrong modified:   prot/0417_cn.sh modified:   prot/0417_ot.sh

--- prot/0417_cn.sh
+++ prot/0417_cn.sh
@@ -8,7 +8,7 @@ for day in 0 1;do
         screen -dmS poller_$hn bash poller.sh $ip $hn $http_sp $https_sp $ss_lp $ss_sp $iperf_dp $iperf_sp
         screen -dmS mtr_${hn}_http bash mtr.sh $ip $hn 80 $http_sp 60 $mtr
         screen -dmS mtr_${hn}_ssl bash mtr.sh $ip $hn 443 $https_sp 60 $mtr
-        screen -dmS mtr_${hn}_ss bash mtr.sh $ip $hn 8838 $ss_lp 60 $mtr
+        screen -dmS mtr_${hn}_ss bash mtr.sh $ip $hn 8388 $ss_lp 60 $mtr
         screen -dmS mtr_${hn}_iperf bash mtr.sh $ip $hn $iperf_dp $iperf_sp 60 $mtr
     done
     sleep 86400
--- prot/0417_ot.sh
+++ prot/0417_ot.sh
@@ -6,7 +6,7 @@ for day in 0 1;do
     cat data/$(hostname)_day${day}.csv | while IFS=' ' read ip hn http_sp https_sp ss_lp ss_sp iperf_dp iperf_sp; do
         screen -dmS mtr_${hn}_http bash mtr.sh $ip $hn $http_sp 80 60 $mtr
         screen -dmS mtr_${hn}_ssl bash mtr.sh $ip $hn $https_sp 443 60 $mtr
-        screen -dmS mtr_${hn}_ss bash mtr.sh $ip $hn $ss_lp 8838 60 $mtr
+        screen -dmS mtr_${hn}_ss bash mtr.sh $ip $hn $ss_lp 8388 60 $mtr
         screen -dmS mtr_${hn}_iperf bash mtr.sh $ip $hn $iperf_sp $iperf_dp 60 $mtr 
     done
     sleep 86400
