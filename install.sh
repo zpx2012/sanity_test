e7e6b8f3427a865ab091416eb4267356836f15a6
zpx2012 <zhupengxiong@yeah.net>
2019-04-25 02:29:07 -0700

    modified:   ../install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install -y curl gcc python-pip python-setuptools libcurl4-gnutls-dev python-dev libgnutls28-dev screen traceroute dos2unix paris-traceroute hping3 nodejs npm automake
 sudo pip install wheel pycurl numpy scipy shadowsocks psutil
 mkdir ~/sanity_test/rs/
-#cd ~/sanity_test/
-#git submodule update --init --recursive
-#cd ~/sanity_test/mtr-insertion/ ; ./install.sh
-#cd ~/sanity_test/mtr-modified/ ; ./install.sh
+cd ~/sanity_test/
+git submodule update --init --recursive
+cd ~/sanity_test/mtr-insertion/ ; ./install.sh
+cd ~/sanity_test/mtr-modified/ ; ./install.sh
