b85eeda035e78436e75d9d6d3ba9013d1b6ee548
zpx2012 <zhupengxiong@yeah.net>
2019-04-26 19:21:11 -0700

    modified:   sched_curl.py

--- sched_curl.py
+++ sched_curl.py
@@ -33,7 +33,8 @@ def main():
     role = sys.argv[6]
     for i in range(0,len(lines)):
         fields = lines[i].split(',')
-        cur_st = datetime.datetime.strptime(fields[4],'%Y-%m-%d %H:%M:%S')
+        cur_st = datetime.datetime.now() + datetime.timedelta(seconds=10)
+        # cur_st = datetime.datetime.strptime(fields[4],'%Y-%m-%d %H:%M:%S')
         if role == 'c':
             sched.add_job(curl_timed, 'interval', args=[fields[0],fields[1],cur_st.strftime('%Y%m%d%H%M'),session,fields[2]], minutes=intvl,
                   start_date=cur_st, end_date=cur_st+datetime.timedelta(days=day))
