d58e8ca642d1c37c1698b105949b49a07fb990a1
zpx2012 <zhupengxiong@yeah.net>
2019-05-02 06:36:27 -0700

    fix

--- prot/8iperf.sh
+++ prot/8iperf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in {0..7};do
-    screen -dmS iperf3_$i bash -c "sudo iperf3 -s -v -p $((5201+i)) -f K 2>&1 | tee -a ~/sanity_test/rs/iperf3_$(hostname)_$((5201+i))_$(date -u +"%m%d%H%M")_server.txt"
+    screen -dmS iperf3_$i bash -c "sudo iperf3 -s -p $((5201+i)) -f K 2>&1 | tee -a ~/sanity_test/rs/iperf3_$(hostname)_$((5201+i))_$(date -u +'%m%d%H%M')_server.txt"
 done
