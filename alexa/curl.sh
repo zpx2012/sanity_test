054a1defd8a9e586439a2980fe8d7c973998a24d
zpx2012 <zhupengxiong@yeah.net>
2019-05-02 00:10:39 -0700

    change 10s to 20s

--- alexa/curl.sh
+++ alexa/curl.sh
@@ -5,6 +5,6 @@ while true;do
     echo
     echo $tag
     echo
-    curl -LJ4kv -o /dev/null --limit-rate 500k -m 10 --speed-time 120 --resolve "$hn:443:$ip" "$url" 2>&1 | tee -a ~/sanity_test/rs/curl_$(hostname)_${tag}_https_${start}.txt
+    curl -LJ4kv -o /dev/null --limit-rate 500k -m 20 --speed-time 120 --resolve "$hn:443:$ip" "$url" 2>&1 | tee -a ~/sanity_test/rs/curl_$(hostname)_${tag}_https_${start}.txt
     done
 done
