f3a939c24dd7c0a16c36f55217a39742dfeb1a08
zpx2012 <zhupengxiong@yeah.net>
2019-05-02 23:20:14 -0700

    modified:   duckdns.sh

--- duckdns.sh
+++ duckdns.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mv ~/sanity_test/duck.log ~/sanity_test/rs/
-out=~/sanity_test/rs/duck.log
+out=~/sanity_test/rs/duck_$(hostname).log
+mv ~/sanity_test/rs/duck.log $out
 while true;do
     echo url="https://www.duckdns.org/update?domains="$(hostname| tr '[:upper:]' '[:lower:]')"-kayodi&token=d349b351-647b-4430-807e-08ad5adacb74&ip=" | curl -k -K - >> $out
     echo | tee -a $out
