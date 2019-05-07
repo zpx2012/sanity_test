c8856e0c05d41fa3786237ef3c6e4deb928169d1
zpx2012 <zhupengxiong@yeah.net>
2019-05-06 19:57:15 -0700

    fix 	modified:   closed-port/0406_other.sh

--- closed-port/0406_other.sh
+++ closed-port/0406_other.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/sanity_test/closed-port
 mtr=~/sanity_test/mtr-modified/mtr
-for day_i in 0 1 2 3; do
+for day_i in 0 1 2 3 4; do
     screen -dmS td bash tcpdump_all.sh
     for p_i in 0 1 2 3;do
         screen -dmS sched_$p_i python sched.py 0505/$(hostname)_d${day_i}_p$(p_i).csv 
