66f4d8efddd8091f0b3b33efd0eb8cbb660e08f2
zpx2012 <zhupengxiong@yeah.net>
2019-05-09 14:28:01 -0700

    fix

--- o2c/0509.sh
+++ o2c/0509.sh
@@ -3,10 +3,10 @@ cd ~/sanity_test/o2c/
 dataf=hk/$(hostname).csv
 
 if [ "$(hostname)" == "HKG-CHN-TEC" ];then
-	screen -dmS td bash ~/sanity_test/ip_scan/tcpdump_server.sh 80
+	screen -dmS td_hk bash ~/sanity_test/ip_scan/tcpdump_server.sh 80
 	screen -dmS sched python sched_curl.py $dataf 65 520 2 s
 else
-	cat $dataf | while IFS=' ' read ip hn sp dp tm; do
+	cat $dataf | while IFS=',' read ip hn sp dp tm; do
 	    screen -dmS td_$hn bash ~/sanity_test/ip_scan/tcpdump_whole.sh $ip $dp $hn c
 	done
     screen -dmS sched python sched_curl.py $dataf 65 520 2 c
