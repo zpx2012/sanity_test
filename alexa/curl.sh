a9809792bda3699164d1af0461ea3e0ed810e87d
zpx2012 <zhupengxiong@yeah.net>
2019-05-13 05:44:27 -0700

    modified:   alexa/curl.sh

--- alexa/curl.sh
+++ alexa/curl.sh
@@ -10,6 +10,6 @@ while true;do
     echo
     echo $tag
     echo
-    curl -LJ4kv -o /dev/null --limit-rate 500k -m 20 --speed-time 120 --resolve "$url" 2>&1 | tee -a ~/sanity_test/rs/curl_$(hostname)_${tag}_https_${start}.txt
+    curl -LJ4kv -o /dev/null --limit-rate 500k -m 20 --speed-time 120 "$url" 2>&1 | tee -a ~/sanity_test/rs/curl_$(hostname)_${tag}_https_${start}.txt
     done
 done
