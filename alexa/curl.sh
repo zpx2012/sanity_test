16129bd62694e4dfe0ba6074ee13367b13648417
zpx2012 <zhupengxiong@yeah.net>
2019-05-28 20:46:17 -0700

    new file:   alexa/0530-urls.csv 	modified:   alexa/curl.sh

--- alexa/curl.sh
+++ alexa/curl.sh
@@ -5,11 +5,15 @@ if [ -z "$1" ]
     exit 1
 fi
 start=$(date -u +"%m%d%H%Mutc")
+mkdir -p ~/sanity_test/rs
 while true;do
-    cat $1 | while IFS=',' read url hn tag ip;do
+    cat $1 | while IFS=',' read url tag;do
     echo
     echo $tag
     echo
-    curl -LJ4kv -o /dev/null --limit-rate 500k -m 20 --speed-time 120 "$url" 2>&1 | tee -a ~/sanity_test/rs/curl_$(hostname)_${tag}_https_${start}.txt
+    out=~/sanity_test/rs/curl_$(hostname)_${tag}_https_${start}.txt
+    date -u +"%Y-%m-%d %H:%M:%S" | tee -a $out
+    echo >> $out
+    curl -LJ4kv -o /dev/null --limit-rate 500k -m 20 --speed-time 120 "$url" 2>&1 | tee -a $out
     done
 done
