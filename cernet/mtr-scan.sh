1a3b0417f4e7d8a33d995034b32eeeb8a87abb0a
zpx2012 <zhupengxiong@yeah.net>
2019-08-02 17:29:10 +0800

    record ip

--- cernet/mtr-scan.sh
+++ cernet/mtr-scan.sh
@@ -1,5 +1,8 @@
 dir=~/sanity_test/cernet
 out=$dir/$(hostname)_cernet.txt
 cat $dir/10ip_perAS.csv | while IFS=',' read ip school;do
+    echo $ip,$school >> $out
     mtr -zwnr4 -c 60 $ip 2>&1 | tee -a $out
+    echo >> $out
+    echo >> $out
 done
