9773c8047c1dd75b51019b932bdb09252ddf393f
zpx2012 <zhupengxiong@yeah.net>
2019-08-02 17:40:42 +0800

    add time to filename

--- cernet/mtr-scan.sh
+++ cernet/mtr-scan.sh
@@ -1,5 +1,5 @@
 dir=~/sanity_test/cernet
-out=$dir/$(hostname)_cernet.txt
+out=$dir/$(hostname)_cernet_$(date -u +%m%d%H%M).txt
 cat $dir/10ip_perAS.csv | while IFS=',' read ip school;do
     echo $ip,$school >> $out
     mtr -zwnr4 -c 60 $ip 2>&1 | tee -a $out
