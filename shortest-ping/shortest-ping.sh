6ac31efb7752cc2bb94c1c12514ca18fa5e417ca
zpx2012 <zhupengxiong@yeah.net>
2019-10-11 16:16:31 -0700

    fix

--- shortest-ping/shortest-ping.sh
+++ shortest-ping/shortest-ping.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-cd ~/sanity_test/ping-test
-out=shortestPing_$(hostname)_$(date -u +"%m%d%H%M").txt
+cd ~/sanity_test/shortest-ping
+out=~/sanity_test/rs/shortestPing_$(hostname)_$(date -u +"%m%d%H%M").txt
 cat $1 | while read ip;do
     ping -c 10 -i 0.5 -q >> $out
     echo >> $out
