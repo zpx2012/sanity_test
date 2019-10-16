de8648cd1f5ad01931ad5511acfd870087acb36c
zpx2012 <zhupengxiong@yeah.net>
2019-10-15 20:48:27 -0700

    add ips-all.txt 	new file:   shortest-ping/ips-all.txt 	modified:   shortest-ping/shortest-ping.sh

--- shortest-ping/shortest-ping.sh
+++ shortest-ping/shortest-ping.sh
@@ -2,6 +2,6 @@
 cd ~/sanity_test/shortest-ping
 out=~/sanity_test/rs/shortestPing_$(hostname)_$(date -u +"%m%d%H%M").txt
 cat $1 | while read ip;do
-    ping -c 10 -i 0.5 -q $ip >> $out
+    ping -W 10 -c 20 -i 0.5 -q $ip >> $out
     echo >> $out
 done
