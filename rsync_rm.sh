71a15655f4f015bca68ab10e314fd7d98f7bd453
zpx2012 <zhupengxiong@yeah.net>
2019-10-28 15:08:42 -0700

    add remove empty subdir 	modified:   rsync_rm.sh

--- rsync_rm.sh
+++ rsync_rm.sh
@@ -2,7 +2,9 @@
 chmod 400 ~/.ssh/id_rsa
 outd=/data/pzhu/st-data/$(hostname)
 ssh -oStrictHostKeyChecking=no pzhu011@terran.cs.ucr.edu mkdir -p $outd
+find ~/sanity_test/rs/ -type d -empty -delete
 while true; do
     rsync -avzS --progress --remove-source-files ~/sanity_test/rs/* pzhu011@terran.cs.ucr.edu:$outd/
+
     sleep 1
 done
