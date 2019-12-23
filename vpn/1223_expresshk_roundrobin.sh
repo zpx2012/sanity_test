e56709fddacb8d2a189f061163d3bf718bd81b26
zpx2012 <zhupengxiong@yeah.net>
2019-12-23 02:27:36 -0800

    add printings

--- vpn/1223_expresshk_roundrobin.sh
+++ vpn/1223_expresshk_roundrobin.sh
@@ -2,12 +2,19 @@ stime=$(date -u +'%Y%m%d%H%M%S')
 mkdir -p ~/sanity_test/rs
 while true;do
     screen -dmS http bash ~/sanity_test/curl_dler.sh 3.86.202.146 VG-AWS-VPN 3000 http $stime
-    screen -S http -X quit 
+    echo HTTP starts
+    screen -ls
     sleep 3005
+    screen -S http -X quit 
+    echo HTTP ends
     screen -dmS vpn openvpn --config hk4-expressvpn.ovpn
-    sleep 10
+    sleep 20
+    echo VPN starts
+    screen -ls
+    ip route
     screen -dmS http bash ~/sanity_test/curl_dler.sh 3.86.202.146 VG-AWS-VPN 3000 vpn $stime
     sleep 3005
     screen -S vpn -X quit
     screen -S http -X quit
+    echo VPN ends
 done
