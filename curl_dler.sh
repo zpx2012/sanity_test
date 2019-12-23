dc72a364802468e513210cee413c78f21a3117c7
zpx2012 <zhupengxiong@yeah.net>
2019-12-23 02:30:42 -0800

    fix

--- curl_dler.sh
+++ curl_dler.sh
@@ -3,6 +3,6 @@ hn=$2
 dur=$3
 prtcl=$4
 stime=$5
-out="~/sanity_test/rs/curl_$(hostname)_"${hn}_${prtcl}_${stime}.txt
+out=~/sanity_test/rs/curl_$(hostname)_${hn}_${prtcl}_${stime}.txt
 echo Start: $(date -u +'%Y-%m-%d %H:%M:%S') >> $out
 curl -LJv4k -o /dev/null --limit-rate 500k -m $dur --speed-time 120 http://$ip/my.mp4 2>&1 | tee -a $out
