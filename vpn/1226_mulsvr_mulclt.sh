28da22383fe60b3c86706a7fd7fad289ec68bba4
zpx2012 <zhupengxiong@yeah.net>
2019-12-26 02:06:47 -0800

    fix

--- vpn/1226_mulsvr_mulclt.sh
+++ vpn/1226_mulsvr_mulclt.sh
@@ -4,7 +4,7 @@ stime=$(date -u +'%Y%m%d%H%M')
 log=~/sanity_test/rs/screenlog_1226MulsvrMulclt_$(hostname)_$(date -u +'%Y%m%d%H%M')
 mkdir -p ~/sanity_test/rs
 while true;do
-    cat ~/sanity_test/vpn/$(hostname).csv | while IFS=',' read ip hn lp; do
+    cat ~/sanity_test/vpn/data/$(hostname).csv | while IFS=',' read ip hn lp; do
         bash ~/sanity_test/vpn/expresshk_roundrobin.sh $ip $hn 60 $lp $stime $log
     done
 done
