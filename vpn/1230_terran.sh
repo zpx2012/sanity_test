b584c8f2a62efcae4bd1ce087ba71d77de2a9455
zpx2012 <zhupengxiong@yeah.net>
2019-12-30 23:05:31 -0800

    fix log filename 	modified:   vpn/1230_terran.sh

--- vpn/1230_terran.sh
+++ vpn/1230_terran.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 stime=$(date -u +'%Y%m%d%H%M')
-log=~/sanity_test/rs/screenlog_1228MulsvrMulclt_$(hostname)_$(date -u +'%Y%m%d%H%M')
+log=~/sanity_test/rs/screenlog_1230terran_$(hostname)_$(date -u +'%Y%m%d%H%M')
 mkdir -p ~/sanity_test/rs
 while true;do
     bash ~/sanity_test/vpn/ucrvpn_roundrobin.sh 169.235.31.181 terran 60 5000 $stime $log
