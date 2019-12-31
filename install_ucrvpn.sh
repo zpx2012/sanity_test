82afd57a2ca5216eae89fdd6547f68f0daa8eb0c
zpx2012 <zhupengxiong@yeah.net>
2019-12-31 01:37:12 -0800

    modified:   install_ucrvpn.sh 	modified:   run.sh

--- install_ucrvpn.sh
+++ install_ucrvpn.sh
@@ -3,4 +3,4 @@ sudo apt-get install -y libpangox-1.0-0 lib32z1 lib32ncurses5
 wget "http://101.133.171.40/anyconnect-linux64-4.6.02074-predeploy-k9.tar.gz"
 tar -xzvf anyconnect-linux64-4.6.02074-predeploy-k9.tar.gz
 cd anyconnect-linux64-4.6.02074/vpn
-./vpn_install.sh
+printf "y\n" | sudo ./vpn_install.sh
--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@
 sudo apt-get update
 sudo apt-get install -y git screen
 cd ~
-screen -dmS run bash -c 'wget http://101.133.171.40/sa.tar.gz;tar -czvf sa.tar.gz;cd sanity_test;bash install.sh'
+screen -dmS run bash -c 'wget http://101.133.171.40/sa.tar.gz;tar -xzvf sa.tar.gz;cd sanity_test;bash install.sh'
 # screen -dmS run bash -c 'git clone --recurse-submodules https://github.com/zpx2012/sanity_test.git;cd sanity_test;bash install.sh'
 # screen -dmS o2c bash o2c/0319_cn.sh $1
 # screen -dmS rsync bash rsync.sh
