5537921c86bb3e4d92a9c84b5977a50c389e22e1
zpx2012 <zhupengxiong@yeah.net>
2020-02-02 20:28:15 -0800

    modified:   vpn/0206_vpn.sh

--- vpn/0206_vpn.sh
+++ vpn/0206_vpn.sh
@@ -9,6 +9,7 @@ mkdir -p ~/sanity_test/rs
 while true;do
     cat ~/sanity_test/vpn/data/0206/$(hostname).csv | while IFS=',' read ip hn lp; do
 		screen -dmS vpn_$hn bash ~/sanity_test/curl_dler_wProxy.sh $ip $hn 60 astrillvpn $stime $((lp+1)) "http://localhost:${proxy_local_sp}"
-        sudo ~/sanity_test/mtr-insertion/mtr -zwnr4T -P 443 -c 60 -i 0.5 $ip 2>&1 | tee -a ~/sanity_test/rs/mtrins_$(hostname)_${hn}_${sp}_tcp_05_60_${stime}.txt
+        screen -dmS mtr_$hn sudo ~/sanity_test/mtr-insertion/mtr -zwnr4T -P 443 -c 60 -i 0.5 $server_ip 2>&1 | tee -a ~/sanity_test/rs/mtrins_$(hostname)_${hn}_$((lp+1))_tcp_05_60_${stime}.txt
+        sleep 60
     done
 done
