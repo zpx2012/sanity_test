b5ad52dc3b93f17d9ac103bf3714428420a68108
zpx2012 <zhupengxiong@yeah.net>
2020-02-03 12:29:54 -0800

    modified:   curl_dler.sh 	modified:   curl_dler_wProxy.sh 	modified:   vpn/0206_astrill.sh 	modified:   vpn/0206_http.sh

--- curl_dler_wProxy.sh
+++ curl_dler_wProxy.sh
@@ -8,10 +8,11 @@ proxy=$7
 out=~/sanity_test/rs/curl_$(hostname)_${hn}_${prtcl}_${stime}.txt
 echo Start: $(date -u +'%Y-%m-%d %H:%M:%S') >> $out
 echo $1 $2 $3 $4 $5 $6 $7
-while true; do
+# while true; do
+for i in 1 2 3 4 5 6 7 8 9 10;do
 	curl -LJv4k -o /dev/null --proxy $proxy --limit-rate 500k -m $dur --speed-time 120 http://$ip/my.mp4 2>&1 | tee singlerun
 	if ! cat singlerun | grep -q 'Address already in use'; then
-		cat singlerun >> $out
+		# cat singlerun >> $out
 		break
 	fi
 	sleep 1 
--- vpn/0206_astrill.sh
+++ vpn/0206_astrill.sh
@@ -4,3 +4,8 @@ cd ~/sanity_test/vpn/
 screen -dmS http bash 0206_http.sh astrill-hk
 sleep 60
 screen -dmS vpn bash 0206_vpn.sh $1 $2 astrill-hk
+
+
+1. Transfer the astrillvpn installation file
+2. Install WSL
+3. Change FIN_Wait2 timeout
