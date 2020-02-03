ec7aa9b07a73a567d22e403d07a9d835615b4b30
zpx2012 <zhupengxiong@yeah.net>
2020-02-02 20:42:46 -0800

    modified:   curl_dler_wProxy.sh

--- curl_dler_wProxy.sh
+++ curl_dler_wProxy.sh
@@ -9,7 +9,7 @@ out=~/sanity_test/rs/curl_$(hostname)_${hn}_${prtcl}_${stime}.txt
 echo Start: $(date -u +'%Y-%m-%d %H:%M:%S') >> $out
 echo $1 $2 $3 $4 $5 $6 $7
 while true; do
-	curl -LJv4k -o /dev/null --proxy $proxy --local-port $lp --limit-rate 500k -m $dur --speed-time 120 http://$ip/my.mp4 2>&1 | tee singlerun
+	curl -LJv4k -o /dev/null --proxy $proxy --limit-rate 500k -m $dur --speed-time 120 http://$ip/my.mp4 2>&1 | tee singlerun
 	if ! cat singlerun | grep -q 'Address already in use'; then
 		cat singlerun >> $out
 		break
