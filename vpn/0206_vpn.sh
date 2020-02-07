1c4f8d0d81d8cd13570a8fe8e262394cd8f20825
zpx2012 <zhupengxiong@yeah.net>
2020-02-07 06:03:05 -0800

    modified:   vpn/0206_vpn.sh

--- vpn/0206_vpn.sh
+++ vpn/0206_vpn.sh
@@ -9,7 +9,7 @@ log=~/sanity_test/rs/screenlog_0206${vpn}_vpn_$(hostname)_$(date -u +'%Y%m%d%H%M
 mkdir -p ~/sanity_test/rs
 while true;do
     cat ~/sanity_test/vpn/data/0206/$(hostname).csv | while IFS=',' read ip hn lp; do
-        # screen -dmS mtr_$hn sudo mtr -zwnr4T -P 443 -c 60 -i 0.5 ${server_ip} 2>&1 | tee -a ~/sanity_test/rs/mtrins_$(hostname)_${hn}_$((lp+1))_tcp_05_60_${stime}.txt
+        screen -dmS mtr_$hn sudo ~/sanity_test/mtr-insertion/mtr -zwnr4T -P 443 -c 60 -i 0.5 ${server_ip} 2>&1 | tee -a ~/sanity_test/rs/mtrins_$(hostname)_${hn}_$((lp+1))_tcp_05_60_${stime}.txt
 		bash ~/sanity_test/curl_dler_wProxy.sh $ip $hn 60 ${vpn}_vpn $stime $((lp+1)) "http://localhost:${proxy_local_sp}"
     done
 done
