b9d5fcb1077029955a9f60b7fe6f3ab7c53a6cd1
zpx2012 <zhupengxiong@yeah.net>
2020-02-07 06:39:23 -0800

    modified:   vpn/install/install_vnc_gnome.sh

--- vpn/install/install_vnc_gnome.sh
+++ vpn/install/install_vnc_gnome.sh
@@ -2,7 +2,7 @@ sudo apt-get install -y vnc4server expect x-window-system-core gdm gnome-panel g
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-desktop
 sudo echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager
 sudo chmod a+x ~/sanity_test/vpn/install/vncserver.exp
-./sanity_test/vpn/install/vncserver.exp
+~/sanity_test/vpn/install/vncserver.exp
 echo '''#!/bin/sh
 # Uncomment the following two lines for normal desktop:
 export XKL_XMODMAP_DISABLE=1
@@ -16,3 +16,4 @@ nautilus &
 gnome-terminal &''' > ~/.vnc/xstartup
 vncserver -kill :1
 vncserver :1
+sudo dpkg -i ~/astrill-setup-linux64.deb
