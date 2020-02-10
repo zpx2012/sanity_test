031492321259e5a76c458dce3dd309a3eec90e58
zpx2012 <zhupengxiong@yeah.net>
2020-02-10 14:37:38 -0800

    [ error ] multiple processs writes to one singlerun 	modified:   curl_dler.sh 	modified:   curl_dler_wProxy.sh

--- curl_dler.sh
+++ curl_dler.sh
@@ -7,12 +7,14 @@ prtcl=$4
 stime=$5
 lp=$6
 out=~/sanity_test/rs/curl_$(hostname)_${hn}_${prtcl}_${stime}.txt
+singlerun=singlerun_${hn}_${prtcl}_$(date -u %Y%m%d%H%M%S)
 echo Start: $(date -u +'%Y-%m-%d %H:%M:%S') >> $out
 # while true; do
 for i in 1 2 3 4 5 6 7 8 9 10;do
-	curl -LJv4k -o /dev/null --local-port $lp --limit-rate 500k -m $dur --speed-time 120 http://$ip/my.mp4 2>&1 | tee singlerun
-	if ! cat singlerun | grep -q 'Address already in use'; then
-		cat singlerun >> $out
+	curl -LJv4k -o /dev/null --local-port $lp --limit-rate 500k -m $dur --speed-time 120 http://$ip/my.mp4 2>&1 | tee $singlerun
+	if ! cat $singlerun | grep -q 'Address already in use'; then
+		cat $singlerun >> $out
+		rm $singlerun
 		break
 	fi
 	sleep 1 
