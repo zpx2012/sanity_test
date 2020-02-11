3422b8339527192ca88dd2a74e6e0f7936e4fd32
zpx2012 <zhupengxiong@yeah.net>
2020-02-11 13:09:16 -0800

    [ fix ] multiple mtr process share single single_mtr 	modified:   vpn/mtr-ins.sh

--- vpn/mtr-ins.sh
+++ vpn/mtr-ins.sh
@@ -13,14 +13,16 @@ if [ $name == '.' ]; then
     name=mtr
 fi
 
+single_mtr=singlerun_${hn}_${name}_$(date -u %Y%m%d%H%M%S)
 out=~/sanity_test/rs/${name}_$(hostname)_${hn}_tcp_${intvl}_${cnt}_${stime}.txt
 
 for i in 1 2 3 4 5 6 7 8 9 10;do
     echo $ip $hn $dp 
-    sudo $mtr -zwnr4T -P $dp -c $cnt -i $intvl ${ip} 2>&1 | tee single_mtr
-    if ! cat single_mtr | grep -q 'send_inserted_tcp_packet:time out'; then
-        cat single_mtr >> $out
+    sudo $mtr -zwnr4T -P $dp -c $cnt -i $intvl ${ip} 2>&1 | tee $single_mtr
+    if ! cat $single_mtr | grep -q 'send_inserted_tcp_packet:time out'; then
+        cat $single_mtr >> $out
         echo >> $out
+        rm $single_mtr
         break
     fi
 done
