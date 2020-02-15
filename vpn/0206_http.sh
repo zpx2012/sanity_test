9dc6108e78d085838b03cb2b56aebb64e9c418be
zpx2012 <zhupengxiong@yeah.net>
2020-02-15 11:38:22 -0800

    modified:   vpn/0206_http.sh 	new file:   vpn/0206_mtr_http.sh 	modified:   vpn/mtr-ins.sh

--- vpn/0206_http.sh
+++ vpn/0206_http.sh
@@ -5,6 +5,6 @@ log=~/sanity_test/rs/screenlog_0206astrill_http_$(hostname)_$(date -u +'%Y%m%d%H
 mkdir -p ~/sanity_test/rs
 while true;do
     cat ~/sanity_test/vpn/data/$1/$(hostname).csv | while IFS=',' read ip hn lp vpn vpn_ip; do
-		bash ~/sanity_test/curl_dler.sh $ip $hn 60 ${vpn}_http $stime $lp
+		  bash ~/sanity_test/curl_dler.sh $ip $hn 60 ${vpn}_http $stime $lp
     done
 done
---
+++ vpn/0206_mtr_http.sh
@@ -0,0 +1,3 @@
+cat ~/sanity_test/vpn/data/$1/$(hostname).csv | while IFS=',' read ip hn lp vpn vpn_ip; do
+    bash ~/sanity_test/vpn/mtr-ins.sh mtr ${ip} $hn 80 60 0.5 $(date -u %Y%m%d%H%M%S)
+done
