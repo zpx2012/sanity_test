97d4199c92f723684ee2b6fee546e23f330f0f61
zpx2012 <zhupengxiong@yeah.net>
2020-02-20 16:53:27 -0800

    [ bug ]

--- vpn/openvpn.sh
+++ vpn/openvpn.sh
@@ -8,13 +8,14 @@ stime=$5
 log=$6
 ovpn=$7
 ovpn_base=$(basename $ovpn)
+vpnname=${ovpn_base%.*}
 con_log=~/openvpn_single_${hn}_$(date +%s)
 
 echo log:$log, ovpn:$ovpn
 
 for i in 1 2 3 4 5;do
     echo $(date +%s)": Try" $i  | tee -a $log
-    screen -dmS vpn_${hn} bash -c "sudo openvpn --config $ovpn --log $con_log;exec bash"
+    screen -dmS vpn_${hn}_$vpnname sudo openvpn --config $ovpn --log $con_log
     for j in {1..30};do
         if cat $con_log | grep -q 'Initialization Sequence Completed'; 
         then
@@ -26,15 +27,16 @@ for i in 1 2 3 4 5;do
     if cat $con_log | grep -q 'Initialization Sequence Completed'; 
     then
         cat $con_log | tee -a $log
+        rm $con_log
         echo $(date +%s)": VPN starts"  | tee -a $log
         echo ----------------------- | tee -a $log
         ip route | tee -a $log
         echo ----------------------- | tee -a $log
-        bash ~/sanity_test/curl_dler.sh $ip $hn $dur ${ovpn_base%.*} $stime $lp
-        screen -S vpn_$hn -X quit
+        bash ~/sanity_test/curl_dler.sh $ip $hn $dur $vpnname $stime $lp
+        screen -S vpn_${hn}_$vpnname -X quit
         echo $(date +%s)":VPN ends" | tee -a $log
         break
     else
-        screen -S vpn_$hn -X quit
+        screen -S vpn_${hn}_$vpnname -X quit
     fi
 done
