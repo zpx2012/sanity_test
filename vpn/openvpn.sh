bb3c8b171d499de5000d14aee4db0cee901c6dbe
zpx2012 <zhupengxiong@yeah.net>
2020-02-25 19:10:37 -0800

    modified:   vpn/openvpn.sh

--- vpn/openvpn.sh
+++ vpn/openvpn.sh
@@ -17,17 +17,17 @@ for i in 1 2 3 4 5;do
     echo $(date +%s)": Try" $i  | tee -a $log
     screen -dmS vpn_${hn}_$vpnname sudo openvpn --config $ovpn --log $con_log
     for j in {1..30};do
-        if cat $con_log | grep -q 'Initialization Sequence Completed'; 
+        if sudo cat $con_log | grep -q 'Initialization Sequence Completed'; 
         then
             break
         fi
         # echo sleep 1 sec | tee -a $log
         sleep 1
     done
-    if cat $con_log | grep -q 'Initialization Sequence Completed'; 
+    if sudo cat $con_log | grep -q 'Initialization Sequence Completed'; 
     then
-        cat $con_log >> $log
-        rm $con_log
+        sudo cat $con_log >> $log
+        sudo rm $con_log
         echo $(date +%s)": VPN starts"  | tee -a $log
         echo ----------------------- | tee -a $log
         ip route | tee -a $log
