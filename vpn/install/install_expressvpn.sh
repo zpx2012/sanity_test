533d3b85df743e0468e284fc5f25a8e93cb23c4d
zpx2012 <zhupengxiong@yeah.net>
2020-03-23 17:46:44 -0700

    [ file ] expressvpn redo 	new file:   expressvpn_wMtr.sh 	new file:   install/expressvpn.exp 	modified:   install/install_expressvpn.sh

--- vpn/install/install_expressvpn.sh
+++ vpn/install/install_expressvpn.sh
@@ -1,5 +1,11 @@
 cd ~
 sudo apt install -y expect
-wget "https://101.133.171.40/expressvpn_2.4.4.19-1_amd64.deb"
+sudo apt autoremove expressvpn
+wget "http://101.133.171.40/expressvpn_2.4.4.19-1_amd64.deb"
 sudo dpkg -i expressvpn_2.4.4.19-1_amd64.deb
-expressvpn preferences set network_lock off
+expressvpn preferences set network_lock off 2> install_expressvpn_preferences_out
+if cat install_expressvpn_preferences_out | grep -q "Please activate your account." ; 
+then 
+	./sanity_test/vpn/install/expressvpn.exp; 
+	expressvpn preferences set network_lock off
+fi
