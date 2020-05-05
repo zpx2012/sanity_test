7a968dd668ffe5674abd5cb81cd97b286b155602
zpx2012 <zhupengxiong@yeah.net>
2020-05-04 23:27:30 -0700

    [bug]

--- curl_dler_url.sh
+++ curl_dler_url.sh
@@ -6,12 +6,14 @@ dur=$3
 prtcl=$4
 stime=$5
 lp=$6
+ip=$7
+dm=$8
 out=~/sanity_test/rs/curl_$(hostname)_${hn}_${prtcl}_${stime}.txt
 singlerun=singlerun_${hn}_${prtcl}_$(date +%s)
 echo Start: $(date -u +'%Y-%m-%d %H:%M:%S') >> $out
 # while true; do
 for i in 1 2 3 4 5 6 7 8 9 10;do
-	curl -LJv4k -o /dev/null --local-port $lp --limit-rate 500k -m $dur --speed-time 120 $url 2>&1 | tee $singlerun
+	curl -LJv4k -o /dev/null --resolve $dm:443:$ip --local-port $lp --limit-rate 500k -m $dur --speed-time 120 $url 2>&1 | tee $singlerun
 	if ! cat $singlerun | grep -q 'Address already in use'; then
 		cat $singlerun >> $out
 		rm $singlerun
--- triple_ack/test_github.sh
+++ triple_ack/test_github.sh
@@ -2,11 +2,11 @@
 
 cd ~/sanity_test/triple_ack
 bash install.sh
-make
+gcc triple_ack.c -o triple_ack -lnfnetlink -lnetfilter_queue
 
-url="https://13.229.189.0/scipy/scipy/zip/master"
+url="https://codeload.github.com/scipy/scipy/zip/master"
 stime=$(date -u +'%Y%m%d%H%M')
-screen -dmS curl_ctl bash -c "while true;do bash ../curl_dler_url.sh $url github-tripleack 120 https $stime 6000;done"
-sleep 120
-screen -dmS curl_ack bash -c "while true;do bash ../curl_dler_url.sh $url github-tripleack 120 https $stime 5000;done"
+screen -dmS curl_ctl bash -c "while true;do bash ../curl_dler_url.sh $url github-ctl 120 https $stime 6000 13.229.189.0 codeload.github.com;done"
+# sleep 120
+screen -dmS curl_ack bash -c "while true;do bash ../curl_dler_url.sh $url github-tripleack 120 https $stime 5000 13.229.189.0 codeload.github.com;done"
 screen -dmS triple_ack ./triple_ack 13.229.189.0 5000
