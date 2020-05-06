fd95d31ed6e0682b64284cc5c2a6be77b9d5c061
zpx2012 <zhupengxiong@yeah.net>
2020-05-06 02:02:05 -0700

    [bug]

--- triple_ack/test_3data.sh
+++ triple_ack/test_3data.sh
@@ -2,17 +2,16 @@
 
 cd 	~/sanity_test/triple_ack/
 gcc triple_ack.c -o triple_ack -lnfnetlink -lnetfilter_queue
-cd data
 if [[ $1 == s ]]; then
 	echo server side
-	cat $(hostname).csv | while IFS=',' read ip hm port cnt; do 
+	cat data/$(hostname).csv | while IFS=',' read ip hm port cnt; do 
 		echo $ip $hm $port $cnt
-		screen -dmS 4data sudo ./triple_ack $ip $port 4 1
+		screen -dmS 4data sudo ~/sanity_test/triple_ack/triple_ack $ip $port 4 1
 	done
 else
 	echo client side
 	stime=$(date -u +'%Y%m%d%H%M')
-	cat $(hostname).csv | while IFS=',' read ip hm;do
+	cat data/$(hostname).csv | while IFS=',' read ip hm;do
 		echo $ip $hm
 		screen -dmS curl_ack bash -c "while true;do bash ~/sanity_test/curl_dler.sh $ip $hm 3data 120 https $stime 5000;done"
 		sleep 120
