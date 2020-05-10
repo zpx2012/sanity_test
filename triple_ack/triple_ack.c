3cd08de22b29725f0500a54dc0ffbaf9d0db6d3d
zpx2012 <zhupengxiong@yeah.net>
2020-05-10 15:55:11 -0700

    [code]

--- triple_ack/triple_ack.c
+++ triple_ack/triple_ack.c
@@ -17,14 +17,15 @@
 
 struct nfq_handle *h;
 struct nfq_q_handle *qh;
-char dst_ip[16];
-unsigned int sport;
-int raw_sd, mode, copy_num;
-const int mark = 3;
+char DST_IP[16];
+unsigned int SPORT;
+int RAW_SD, MODE, COPY_NUM;
+const int MARK = 3;
 
 struct thread_data{
-   int  len;
-   char *buf;
+        int  id_rvs;
+        int  len;
+        unsigned char *buf;
 };
 
 static u_int32_t print_pkt (struct nfq_data *tb)
@@ -127,59 +128,91 @@ int send_raw_packet(size_t sd, unsigned char *buf, uint16_t len) {
 static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data)
 {
         // u_int32_t id = print_pkt(nfa);
-        u_int32_t id;
-        unsigned char* packet_data;
-        int packet_len, i;
+        unsigned char* packet;
+        u_int32_t id_rvs = nfq_get_msg_packet_hdr(nfa)->packet_id;
+        int packet_len = nfq_get_payload(nfa, &packet);
 
-        printf("entering callback:%p %p\n", nfmsg, nfa);
-        struct nfqnl_msg_packet_hdr *ph;
-        ph = nfq_get_msg_packet_hdr(nfa);    
-        id = ntohl(ph->packet_id);
+        struct thread_data* thr_data = malloc(sizeof(struct thread_data));
+        if (!thr_data)
+        {
+                fprintf(stderr, "error during thr_data malloc\n");
+                return -1;                                /* code */
+        }
+        thr_data->id_rvs = id_rvs;
+        thr_data->len = packet_len;
+        thr_data->buf = (unsigned char *)malloc(packet_len);
+        if (!thr_data->buf){
+                fprintf(stderr, "error during thr_data->buf malloc\n");
+                return -1;
+        }
+        strncpy(thr_data->buf, packet, packet_len);
+        if(thr_pool_queue(pool, pool_handler, (void *)thr_data) < 0){
+                fprintf(stderr, "error during thr_pool_queue\n");
+                return -1;
+        }
 
-        packet_len = nfq_get_payload(nfa, &packet_data);
-        struct iphdr *iph = (struct iphdr*)packet_data;          /* IPv4 header */
+        return 0;
+
+        // printf("entering callback\n");
+        // struct nfqnl_msg_packet_hdr *ph;
+        // ph = nfq_get_msg_packet_hdr(nfa);    
+        // id = ntohl(ph->packet_id);
+        // packet_len = nfq_get_payload(nfa, &packet_data);
+}
+
+void pool_handler(struct thread_data* thr_data){
+        u_int32_t id = ntohl(thr_data->id_rvs);
+        unsigned int packet_len = thr_data->len;
+        unsigned char* packet = thr_data->buf;
+        struct iphdr  *iph = (struct iphdr*)packet_data;                 /* IPv4 header */
         struct tcphdr *tcph = (struct tcphdr*)(packet_data + 20);        /* TCP header */
         unsigned short payload_len = ntohs(iph->tot_len) - iph->ihl*4 - tcph->doff*4;
+
         // printf("payload_len:%d\n", payload_len);
-        if ((!payload_len && !mode) || (payload_len && mode)){
-                printf("sent %d packets len = %d.\n", copy_num, payload_len);
+        if ((!payload_len && !MODE) || (payload_len && MODE)){
+                printf("sent %d packets len = %d.\n", COPY_NUM, payload_len);
                 // print_tcp_packet(packet_data);   
-                for (int i = 0; i < copy_num; ++i)
+                for (int i = 0; i < COPY_NUM; ++i)
                 {
-                        send_raw_packet(raw_sd, packet_data, packet_len);
+                        send_raw_packet(RAW_SD, packet_data, packet_len);
                 }
         }
-        return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+        if (nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL) < 0){
+                fprintf(stderr, "error during nfq_set_verdict\n");
+        }
+        printf("free %p, %p\n", thr_data, thr_data->buf);
+        free(thr_data->buf);
+        free(thr_data);
 }
 
 void add_iprules(){
         char buf[200];
-        if (mode == 0){
-                snprintf(buf,200,"iptables -A OUTPUT -d %s --protocol tcp --sport %d --tcp-flags ACK ACK -m mark --mark %d -j ACCEPT", dst_ip, sport, mark); 
+        if (MODE == 0){
+                snprintf(buf,200,"iptables -A OUTPUT -d %s --protocol tcp --sport %d --tcp-flags ACK ACK -m mark --mark %d -j ACCEPT", DST_IP, SPORT, MARK); 
                 system(buf);
-                snprintf(buf,200,"iptables -A OUTPUT -d %s --protocol tcp --sport %d --tcp-flags ACK ACK -j NFQUEUE", dst_ip, sport); 
+                snprintf(buf,200,"iptables -A OUTPUT -d %s --protocol tcp --sport %d --tcp-flags ACK ACK -j NFQUEUE", DST_IP, SPORT); 
                 system(buf);
         }
-        else if (mode == 1){
-                snprintf(buf,200,"iptables -A OUTPUT -d %s --protocol tcp --dport %d --tcp-flags ACK ACK -m mark --mark %d -j ACCEPT", dst_ip, sport, mark); 
+        else if (MODE == 1){
+                snprintf(buf,200,"iptables -A OUTPUT -d %s --protocol tcp --dport %d --tcp-flags ACK ACK -m mark --mark %d -j ACCEPT", DST_IP, SPORT, MARK); 
                 system(buf);
-                snprintf(buf,200,"iptables -A OUTPUT -d %s --protocol tcp --dport %d --tcp-flags ACK ACK -j NFQUEUE", dst_ip, sport); 
+                snprintf(buf,200,"iptables -A OUTPUT -d %s --protocol tcp --dport %d --tcp-flags ACK ACK -j NFQUEUE", DST_IP, SPORT); 
                 system(buf);               
         }
 }
 
 void delete_iprules(){
         char buf[200];
-        if (mode == 0){
-                snprintf(buf,200,"iptables -D OUTPUT -d %s --protocol tcp --sport %d --tcp-flags ACK ACK -m mark --mark %d -j ACCEPT", dst_ip, sport, mark); 
+        if (MODE == 0){
+                snprintf(buf,200,"iptables -D OUTPUT -d %s --protocol tcp --sport %d --tcp-flags ACK ACK -m mark --mark %d -j ACCEPT", DST_IP, SPORT, MARK); 
                 system(buf);
-                snprintf(buf,200,"iptables -D OUTPUT -d %s --protocol tcp --sport %d --tcp-flags ACK ACK -j NFQUEUE", dst_ip, sport); 
+                snprintf(buf,200,"iptables -D OUTPUT -d %s --protocol tcp --sport %d --tcp-flags ACK ACK -j NFQUEUE", DST_IP, SPORT); 
                 system(buf);
         }
-        else if (mode == 1){
-                snprintf(buf,200,"iptables -D OUTPUT -d %s --protocol tcp --dport %d --tcp-flags ACK ACK -m mark --mark %d -j ACCEPT", dst_ip, sport, mark); 
+        else if (MODE == 1){
+                snprintf(buf,200,"iptables -D OUTPUT -d %s --protocol tcp --dport %d --tcp-flags ACK ACK -m mark --mark %d -j ACCEPT", DST_IP, SPORT, MARK); 
                 system(buf);
-                snprintf(buf,200,"iptables -D OUTPUT -d %s --protocol tcp --dport %d --tcp-flags ACK ACK -j NFQUEUE", dst_ip, sport); 
+                snprintf(buf,200,"iptables -D OUTPUT -d %s --protocol tcp --dport %d --tcp-flags ACK ACK -j NFQUEUE", DST_IP, SPORT); 
                 system(buf);               
         }
 }
@@ -210,12 +243,6 @@ void INThandler(int sig)
         exit(0);
 }
 
-void pool_handler(struct thread_data* thr_data){
-        nfq_handle_packet(h, thr_data->buf, thr_data->len);
-        printf("free %p, %p\n", thr_data, thr_data->buf);
-        free(thr_data->buf);
-        free(thr_data);
-}
 
 
 int main(int argc, char **argv)
@@ -225,17 +252,16 @@ int main(int argc, char **argv)
         int fd;
         int rv;
         char buf[4096] __attribute__ ((aligned));
-        struct thread_data* thr_data;
 
         if (argc < 5){
-                printf("Usage: dst_ip client_port copy_num mode[0:client/1:server]\n");
+                printf("Usage: dst_ip client_port COPY_NUM MODE[0:client/1:server]\n");
                 exit(0);
         }
-        strcpy(dst_ip, argv[1]);
-        dst_ip[15] = '\0';
-        sport = atoi(argv[2]);
-        copy_num = atoi(argv[3]);
-        mode = atoi(argv[4]);
+        strcpy(DST_IP, argv[1]);
+        DST_IP[15] = '\0';
+        SPORT = atoi(argv[2]);
+        COPY_NUM = atoi(argv[3]);
+        MODE = atoi(argv[4]);
 
         add_iprules();
 
@@ -247,13 +273,13 @@ int main(int argc, char **argv)
 
         //setup of raw socket to send packets
         printf("setting up raw socket\n");
-        raw_sd = socket(PF_INET, SOCK_RAW, IPPROTO_RAW);
-        if(raw_sd < 0) {
+        RAW_SD = socket(PF_INET, SOCK_RAW, IPPROTO_RAW);
+        if(RAW_SD < 0) {
                 fprintf(stderr, "couldn't open RAW socket\n");
                 exit(1);
         }
 
-        if (setsockopt(raw_sd, SOL_SOCKET, SO_MARK, &mark, sizeof(mark)) < 0)
+        if (setsockopt(RAW_SD, SOL_SOCKET, SO_MARK, &MARK, sizeof(MARK)) < 0)
         {
                 fprintf(stderr, "couldn't set MARK\n");
                 exit(1);
@@ -285,9 +311,9 @@ int main(int argc, char **argv)
                 exit(1);
         }
 
-        printf("setting copy_packet mode\n");
-        if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
-                fprintf(stderr, "can't set packet_copy mode\n");
+        printf("setting copy_packet MODE\n");
+        if (nfq_set_MODE(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+                fprintf(stderr, "can't set packet_copy MODE\n");
                 exit(1);
         }
 
@@ -297,24 +323,8 @@ int main(int argc, char **argv)
         for (;;) {
                 if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
                         printf("pkt received\n");
-                        // thr_data = malloc(sizeof(struct thread_data));
-                        // if (!thr_data)
-                        // {
-                        //         fprintf(stderr, "error during thr_data malloc\n");
-                        //         continue;                                /* code */
-                        // }
-                        // thr_data->len = rv;
-                        // thr_data->buf = (char *)malloc(rv);
-                        // if (!thr_data->buf){
-                        //         fprintf(stderr, "error during thr_data->buf malloc\n");
-                        //         continue;
-                        // }
-                        // strncpy(thr_data->buf, buf, rv);
-                        // if(thr_pool_queue(pool, pool_handler, (void *)thr_data) < 0){
-                        //         fprintf(stderr, "error during thr_pool_queue\n");
-                        //         continue;
-                        // }
-                        // printf("before nfq_handle_packet: buf %p\n", buf);
+
+                        printf("before nfq_handle_packet: buf %p\n", buf);
                         nfq_handle_packet(h, buf, rv);
                         // printf("after nfq_handle_packet\n");
                         continue;
