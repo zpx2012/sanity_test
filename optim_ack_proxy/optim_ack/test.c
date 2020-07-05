a918d66a21781221cfb2ce2d8f6fe6cc3049d0c8
zpx2012 <zhupengxiong@yeah.net>
2020-07-04 22:48:47 -0700

    [bug] print ack

--- optim_ack_proxy/optim_ack/test.c
+++ optim_ack_proxy/optim_ack/test.c
@@ -819,8 +819,10 @@ void* optimistic_ack(void* threadid){
 
     log_exp("Optim ack starts");
     for (int k = 0; !optim_ack_stop ; k++){
-        for (int i = 0; i < SUBCONN_NUM; i++)
+        for (int i = 0; i < SUBCONN_NUM; i++){
             send_ACK("",subconn_infos[i].ini_seq_rem+1+k*payload_len, subconn_infos[i].cur_seq_loc, subconn_infos[i].local_port);
+            log_exp("\nS%d: ack %u sent\n", i, 1+k*payload_len);
+        }
         usleep(ack_pacing);
     }
     log_exp("Optim ack ends");
