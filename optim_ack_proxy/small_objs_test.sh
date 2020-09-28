dce0021c664246cbacad92e906054e0859d06b83
zpx2012 <zhupengxiong@yeah.net>
2020-09-28 01:45:11 -0700

    [code] add normal mode

--- optim_ack_proxy/small_objs_test.sh
+++ optim_ack_proxy/small_objs_test.sh
@@ -3,10 +3,16 @@
 log=~/sanity_test/optim_ack_proxy/rs/small_objs_$(date -u +%m%d%H%M).txt
 while true; do
 	cat ~/sanity_test/optim_ack_proxy/small_objs.txt | while read obj; do
+		echo $(date -u --rfc-3339=ns): Curl download start - Normal mode 2>&1 | tee -a $log
+		start=$(date +%s.%N)
+		curl -LJ4k -o /dev/null --speed-time 5 http://mirror.math.princeton.edu/pub/ubuntu/indices/$obj 2>&1 | tee -a $log
+		duration=$(echo "$(date +%s.%N) - $start" | bc)
+		echo $(date -u --rfc-3339=ns): Curl download end, duration $duration 2>&1 | tee -a $log
+		echo "---" | tee -a $log
 		sudo /usr/local/squid/sbin/squid -s
 		echo $(date -u --rfc-3339=ns): Start squid 2>&1 | tee -a $log
 		sleep 2
-		echo $(date -u --rfc-3339=ns): Curl download start 2>&1 | tee -a $log
+		echo $(date -u --rfc-3339=ns): Curl download start - Proxy mode 2>&1 | tee -a $log
 		start=$(date +%s.%N)
 		curl -LJ4k -o /dev/null -x http://127.0.0.1:3128 --speed-time 5 http://mirror.math.princeton.edu/pub/ubuntu/indices/$obj 2>&1 | tee -a $log
 		duration=$(echo "$(date +%s.%N) - $start" | bc)
@@ -17,6 +23,8 @@ while true; do
 		sleep 5
 		echo $(date -u --rfc-3339=ns): Wait 29s for squid to stop 2>&1 | tee -a $log
 		ps -ef | grep squid 2>&1 | tee -a $log
+		echo | tee -a $log
+		echo | tee -a $log
 	done
 done
 	
